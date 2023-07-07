3ba4e8806b0ff80cb465c290252fcaa9882f9aa3
Wonyong Kim(Ryan Kim) <ryan@lightscale.io>
2023-07-07 16:28:13 +0900

    feat(base): add FilePath class

---
+++ tachyon/base/files/file_path.cc
@@ -0,0 +1,215 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "tachyon/base/files/file_path.h"
+
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_join.h"
+
+#include "tachyon/base/logging.h"
+#include "tachyon/base/strings/string_util.h"
+#include "tachyon/build/build_config.h"
+
+namespace tachyon {
+namespace base {
+namespace {
+
+#if defined(OS_POSIX)
+constexpr const char* kRootPath = "/";
+constexpr const char* kSeparator = "/";
+constexpr const char* kExtensionSeparator = ".";
+constexpr const char* kParentDirectory = "..";
+#else
+#error Unsupported platform
+#endif
+
+bool AreAllSeparators(const std::string& input) {
+  for (char it : input) {
+    if (it != kSeparator[0]) return false;
+  }
+
+  return true;
+}
+
+}  // namespace
+
+FilePath::FilePath() = default;
+
+FilePath::FilePath(std::string_view path) : path_(std::string(path)) {}
+
+FilePath::FilePath(const FilePath& other) = default;
+FilePath& FilePath::operator=(const FilePath& other) = default;
+
+FilePath::FilePath(FilePath&& other) noexcept = default;
+FilePath& FilePath::operator=(FilePath&& other) noexcept = default;
+
+FilePath::~FilePath() = default;
+
+bool FilePath::IsRoot() const {
+  return path_.length() > 0 && AreAllSeparators(path_);
+}
+
+void FilePath::GetComponents(std::vector<std::string>* components) const {
+  DCHECK(components);
+  if (!components) return;
+  components->clear();
+  if (value().empty()) return;
+
+  std::vector<std::string> ret_val;
+  FilePath current = *this;
+  FilePath base;
+
+  // Capture path components.
+  while (current != current.DirName()) {
+    base = current.BaseName();
+    if (!AreAllSeparators(base.value())) ret_val.push_back(base.value());
+    current = current.DirName();
+  }
+
+  // Capture root, if any.
+  if (current.IsRoot()) ret_val.push_back(kRootPath);
+
+  *components = std::vector<std::string>(ret_val.rbegin(), ret_val.rend());
+}
+
+bool FilePath::IsParent(const FilePath& child) const {
+  return AppendRelativePath(child, nullptr);
+}
+
+bool FilePath::AppendRelativePath(const FilePath& child, FilePath* path) const {
+  std::vector<std::string> parent_components;
+  std::vector<std::string> child_components;
+  GetComponents(&parent_components);
+  child.GetComponents(&child_components);
+
+  if (parent_components.empty() ||
+      parent_components.size() >= child_components.size())
+    return false;
+
+  std::vector<std::string>::const_iterator parent_comp =
+      parent_components.begin();
+  std::vector<std::string>::const_iterator child_comp =
+      child_components.begin();
+
+  while (parent_comp != parent_components.end()) {
+    if (*parent_comp != *child_comp) return false;
+    ++parent_comp;
+    ++child_comp;
+  }
+
+  if (path) {
+    *path = path->Append(FilePath(
+        absl::StrJoin(child_comp, child_components.cend(), kSeparator)));
+  }
+  return true;
+}
+
+FilePath FilePath::DirName() const {
+  std::string_view p = path_;
+  size_t last_separator = p.find_last_of(kSeparator, std::string::npos);
+  if (last_separator == std::string::npos) {
+    return FilePath();
+  } else if (last_separator == 0) {
+    // root directory
+    return FilePath(kRootPath);
+  } else {
+    return FilePath(p.substr(0, last_separator));
+  }
+}
+
+FilePath FilePath::BaseName() const {
+  std::string_view p = path_;
+  size_t last_separator = p.find_last_of(kSeparator, std::string::npos);
+  if (last_separator == std::string::npos) {
+    return FilePath(path_);
+  }
+
+  return FilePath(
+      p.substr(last_separator + 1, p.length() - last_separator - 1));
+}
+
+std::string FilePath::Extension() const {
+  FilePath base(BaseName());
+  std::string_view p = base.value();
+  size_t last_separator =
+      p.find_last_of(kExtensionSeparator, std::string::npos);
+  if (last_separator == std::string::npos) return EmptyString();
+  return std::string(p.substr(last_separator, p.length() - last_separator));
+}
+
+FilePath FilePath::Append(std::string_view component) const {
+  return Append(FilePath(component));
+}
+
+FilePath FilePath::Append(const FilePath& component) const {
+  if (component.IsAbsolute()) return FilePath(EmptyString());
+  if (path_.empty()) return component;
+  if (EndsWithSeparator()) {
+    return FilePath(absl::StrCat(path_, component.value()));
+  }
+  return FilePath(absl::StrJoin({path_, component.value()}, kSeparator));
+}
+
+FilePath FilePath::operator/(std::string_view component) const {
+  return Append(component);
+}
+
+FilePath FilePath::operator/(const FilePath& component) const {
+  return Append(component);
+}
+
+FilePath& FilePath::operator/=(std::string_view component) {
+  *this = Append(component);
+  return *this;
+}
+
+FilePath& FilePath::operator/=(const FilePath& component) {
+  *this = Append(component);
+  return *this;
+}
+
+bool FilePath::IsAbsolute() const { return StartsWith(path_, kSeparator); }
+
+bool FilePath::EndsWithSeparator() const {
+  if (path_.empty()) return false;
+  return EndsWith(path_, kSeparator);
+}
+
+FilePath FilePath::AsEndingWithSeparator() const {
+  if (EndsWithSeparator() || path_.empty()) return *this;
+
+  std::string path_str;
+  path_str.reserve(path_.length() + 1);  // Only allocate string once.
+  path_str = path_;
+  path_str.push_back(kSeparator[0]);
+  return FilePath(path_str);
+}
+
+FilePath FilePath::StripTrailingSeparators() const {
+  FilePath new_path(path_);
+  new_path.StripTrailingSeparatorsInternal();
+
+  return new_path;
+}
+
+void FilePath::StripTrailingSeparatorsInternal() {
+  size_t pos = path_.find_last_not_of(kSeparator);
+  if (pos == std::string::npos) {
+    path_.resize(1);
+  } else {
+    path_.resize(pos + 1);
+  }
+}
+
+bool FilePath::ReferencesParent() const {
+  return path_.find(kParentDirectory) != std::string::npos;
+}
+
+std::ostream& operator<<(std::ostream& out, const FilePath& file_path) {
+  out << file_path.value();
+  return out;
+}
+
+}  // namespace base
+}  // namespace tachyon
---
+++ tachyon/base/files/file_path.h
@@ -0,0 +1,139 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TACHYON_BASE_FILES_FILE_PATH_H_
+#define TACHYON_BASE_FILES_FILE_PATH_H_
+
+#include <ostream>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "tachyon/export.h"
+
+namespace tachyon {
+namespace base {
+
+// This is a class represents file path inside tachyon projects.
+// CAUTION: On linux, unlike your expectation, it's not utf-8 encoded, encoding
+// is dependent to locale.
+// NOTE: please do not use std::string directly!
+class TACHYON_EXPORT FilePath {
+ public:
+  FilePath();
+  explicit FilePath(std::string_view path);
+
+  FilePath(const FilePath& other);
+  FilePath& operator=(const FilePath& other);
+
+  FilePath(FilePath&& other) noexcept;
+  FilePath& operator=(FilePath&& other) noexcept;
+
+  ~FilePath();
+
+  bool operator==(const FilePath& other) const { return path_ == other.path_; }
+  bool operator!=(const FilePath& other) const { return path_ != other.path_; }
+
+  bool operator<(const FilePath& other) const { return path_ < other.path_; }
+
+  const std::string& value() const { return path_; }
+
+  bool empty() const { return path_.empty(); }
+
+  void clear() { path_.clear(); }
+
+  // Returns true if |path_| references to root. It also returns true |path_|
+  // consists of '/';
+  bool IsRoot() const;
+
+  // Fills |components| with all of the components of |path_|. It is
+  // equivalent to calling DirName().value() on the path's root component,
+  // and BaseName().value() on each child component.
+  //
+  // To make sure this is lossless so we can differentiate absolute and
+  // relative paths, the root slash will be included even though no other
+  // slashes will be. So for "/foo/bar", |components| will be [ "/", "foo",
+  // "bar" ]
+  void GetComponents(std::vector<std::string>* components) const;
+
+  // Returns true if this FilePath is a parent or ancestor of the |child|.
+  // Absolute and relative paths are accepted i.e. /foo is a parent to /foo/bar,
+  // and foo is a parent to foo/bar. Any ancestor is considered a parent i.e. /a
+  // is a parent to both /a/b and /a/b/c.  Does not convert paths to absolute,
+  // follow symlinks or directory navigation (e.g. ".."). A path is *NOT* its
+  // own parent.
+  bool IsParent(const FilePath& child) const;
+
+  // If IsParent(child) holds, appends to path (if non-NULL) the
+  // relative path to child and returns true.  For example, if parent
+  // holds "/Users/johndoe/Library/Application Support", child holds
+  // "/Users/johndoe/Library/Application Support/Google/Chrome/Default", and
+  // *path holds "/Users/johndoe/Library/Caches", then after
+  // parent.AppendRelativePath(child, path) is called *path will hold
+  // "/Users/johndoe/Library/Caches/Google/Chrome/Default".  Otherwise,
+  // returns false.
+  bool AppendRelativePath(const FilePath& child, FilePath* path) const;
+
+  // Returns the directory name of this FilePath. If |path_| is root, it
+  // returns the same. For example, if |path_| is "foo/bar", it returns "foo".
+  [[nodiscard]] FilePath DirName() const;
+
+  // Returns the base name of this FilePath. If |path_| is root, it returns
+  // empty path. For example, if |path_| is "foo/bar", it returns "bar".
+  [[nodiscard]] FilePath BaseName() const;
+
+  // Returns the extension of this FilePath. If |path_| doesn't contain an
+  // extension, it returns empty string. For example, if |path_| is "foo.jpg",
+  // it returns ".jpg".
+  [[nodiscard]] std::string Extension() const;
+
+  // Returns a FilePath by appending a separator and the supplied path
+  // component to this object's path. Append takes care to avoid adding
+  // excessive separators if this object's path already ends with a separator.
+  //
+  // NOTE: |component| must be a relative path, it is an error to pass an
+  // absolute path.
+  [[nodiscard]] FilePath Append(std::string_view component) const;
+  [[nodiscard]] FilePath Append(const FilePath& component) const;
+
+  // Convenient operators, which does the same above.
+  FilePath operator/(std::string_view component) const;
+  FilePath operator/(const FilePath& component) const;
+  FilePath& operator/=(std::string_view component);
+  FilePath& operator/=(const FilePath& component);
+
+  // Returns true if |path_| is absolute.
+  bool IsAbsolute() const;
+
+  // Returns true if |path_| ends with a path separator character.
+  [[nodiscard]] bool EndsWithSeparator() const;
+
+  // Returns a copy of this FilePath that ends with a trailing separator. If
+  // the input path is empty, an empty FilePath will be returned.
+  [[nodiscard]] FilePath AsEndingWithSeparator() const;
+
+  // Returns a copy of this FilePath that does not end with a trailing
+  // separator.
+  [[nodiscard]] FilePath StripTrailingSeparators() const;
+
+  // Returns true if this FilePath contains an attempt to reference a parent
+  // directory (e.g. has a path component that is "..").
+  bool ReferencesParent() const;
+
+ private:
+  // Remove trailing separators from this object. If |path_| is absolute, it
+  // will never be stripped any more than to refer to the absolute root
+  // directory, so "////" will become "/", not "".
+  void StripTrailingSeparatorsInternal();
+
+  std::string path_;
+};
+
+TACHYON_EXPORT std::ostream& operator<<(std::ostream& out,
+                                        const FilePath& file_path);
+
+}  // namespace base
+}  // namespace tachyon
+
+#endif  // TACHYON_BASE_FILES_FILE_PATH_H_
