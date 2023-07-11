a9bf1ba94e42095d462a2dafbc77b88c4042a23e
Wonyong Kim(Ryan Kim) <ryan@lightscale.io>
2023-07-11 16:18:29 +0900

    feat(base): add cxx20_erase files

---
+++ tachyon/base/containers/cxx20_erase_internal.h
@@ -0,0 +1,37 @@
+// Copyright 2021 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TACHYON_BASE_CONTAINERS_CXX20_ERASE_INTERNAL_H_
+#define TACHYON_BASE_CONTAINERS_CXX20_ERASE_INTERNAL_H_
+
+#include <stddef.h>
+
+// Internal portion of base/containers/cxx20_erase_*.h. Please include those
+// headers instead of including this directly.
+
+namespace tachyon {
+namespace base {
+
+namespace internal {
+
+// Calls erase on iterators of matching elements and returns the number of
+// removed elements.
+template <typename Container, typename Predicate>
+size_t IterateAndEraseIf(Container& container, Predicate pred) {
+  size_t old_size = container.size();
+  for (auto it = container.begin(), last = container.end(); it != last;) {
+    if (pred(*it))
+      it = container.erase(it);
+    else
+      ++it;
+  }
+  return old_size - container.size();
+}
+
+}  // namespace internal
+
+}  // namespace base
+}  // namespace tachyon
+
+#endif  // TACHYON_BASE_CONTAINERS_CXX20_ERASE_INTERNAL_H_
