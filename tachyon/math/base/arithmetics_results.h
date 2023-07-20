13356f054847832894afc903fde76275eda4bbc4
Wonyong Kim(Ryan Kim) <ryan@lightscale.io>
2023-07-20 20:56:35 +0900

    chore(math): add constexpr to ArithmeticsResults

--- tachyon/math/base/arithmetics_results.h
+++ tachyon/math/base/arithmetics_results.h
@@ -13,10 +13,10 @@ struct AddResult {
   T result = 0;
   T carry = 0;
 
-  bool operator==(const AddResult& other) const {
+  constexpr bool operator==(const AddResult& other) const {
     return result == other.result && carry == other.carry;
   }
-  bool operator!=(const AddResult& other) const { return !operator==(other); }
+  constexpr bool operator!=(const AddResult& other) const { return !operator==(other); }
 
   std::string ToString() const {
     return absl::Substitute("($0, $1)", result.ToString(), carry.ToString());
@@ -33,10 +33,10 @@ struct SubResult {
   T result = 0;
   T borrow = 0;
 
-  bool operator==(const SubResult& other) const {
+  constexpr bool operator==(const SubResult& other) const {
     return result == other.result && borrow == other.borrow;
   }
-  bool operator!=(const SubResult& other) const { return !operator==(other); }
+  constexpr bool operator!=(const SubResult& other) const { return !operator==(other); }
 
   std::string ToString() const {
     return absl::Substitute("($0, $1)", result.ToString(), borrow.ToString());
@@ -53,10 +53,10 @@ struct MulResult {
   T hi = 0;
   T lo = 0;
 
-  bool operator==(const MulResult& other) const {
+  constexpr bool operator==(const MulResult& other) const {
     return hi == other.hi && lo == other.lo;
   }
-  bool operator!=(const MulResult& other) const { return !operator==(other); }
+  constexpr bool operator!=(const MulResult& other) const { return !operator==(other); }
 
   std::string ToString() const {
     return absl::Substitute("($0, $1)", hi.ToString(), lo.ToString());
@@ -73,10 +73,10 @@ struct DivResult {
   T quotient = 0;
   T remainder = 0;
 
-  bool operator==(const DivResult& other) const {
+  constexpr bool operator==(const DivResult& other) const {
     return quotient == other.quotient && remainder == other.remainder;
   }
-  bool operator!=(const DivResult& other) const { return !operator==(other); }
+  constexpr bool operator!=(const DivResult& other) const { return !operator==(other); }
 
   std::string ToString() const {
     return absl::Substitute("($0, $1)", quotient.ToString(),
