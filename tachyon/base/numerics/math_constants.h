8f0fe5a85e6e31ca1ec8482389448e886e79e820
Wonyong Kim(Ryan Kim) <ryan@lightscale.io>
2023-07-26 10:27:15 +0900

    feat(base): add numerics

---
+++ tachyon/base/numerics/math_constants.h
@@ -0,0 +1,33 @@
+// Copyright 2017 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TACHYON_BASE_NUMERICS_MATH_CONSTANTS_H_
+#define TACHYON_BASE_NUMERICS_MATH_CONSTANTS_H_
+
+namespace tachyon {
+namespace base {
+
+constexpr double kPiDouble = 3.14159265358979323846;
+constexpr float kPiFloat = 3.14159265358979323846f;
+
+// pi/180 and 180/pi. These are correctly rounded from the true
+// mathematical value, unlike what you'd get from e.g.
+// 180.0f / kPiFloat.
+constexpr double kDegToRadDouble = 0.017453292519943295769;
+constexpr float kDegToRadFloat = 0.017453292519943295769f;
+constexpr double kRadToDegDouble = 57.295779513082320876798;
+constexpr float kRadToDegFloat = 57.295779513082320876798f;
+
+// sqrt(1/2) = 1/sqrt(2).
+constexpr double kSqrtHalfDouble = 0.70710678118654752440;
+constexpr float kSqrtHalfFloat = 0.70710678118654752440f;
+
+// The mean acceleration due to gravity on Earth in m/s^2.
+constexpr double kMeanGravityDouble = 9.80665;
+constexpr float kMeanGravityFloat = 9.80665f;
+
+}  // namespace base
+}  // namespace tachyon
+
+#endif  // TACHYON_BASE_NUMERICS_MATH_CONSTANTS_H_
---
+++ tachyon/base/numerics/safe_math.h
@@ -0,0 +1,12 @@
+// Copyright 2017 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TACHYON_BASE_NUMERICS_SAFE_MATH_H_
+#define TACHYON_BASE_NUMERICS_SAFE_MATH_H_
+
+#include "tachyon/base/numerics/checked_math.h"
+#include "tachyon/base/numerics/clamped_math.h"
+#include "tachyon/base/numerics/safe_conversions.h"
+
+#endif  // TACHYON_BASE_NUMERICS_SAFE_MATH_H_
