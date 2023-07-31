bbb5d78b3efb5a6b855bb998d0e0500d178717ce
Wonyong Kim(Ryan Kim) <ryan@lightscale.io>
2023-07-31 12:25:07 +0900

    test(math): add benchmark for goldilocks prime

--- tachyon/math/finite_fields/goldilocks_prime/goldilocks.h
+++ tachyon/math/finite_fields/goldilocks_prime/goldilocks.h
@@ -1,14 +1,21 @@
 #ifndef TACHYON_MATH_FINITE_FIELDS_GOLDILOCKS_PRIME_GOLDILOCKS_H_
 #define TACHYON_MATH_FINITE_FIELDS_GOLDILOCKS_PRIME_GOLDILOCKS_H_
 
+#if defined(TACHYON_GOLDILOCKS_BACKEND)
 #include "tachyon/math/finite_fields/goldilocks_prime/prime_field_goldilocks.h"
+#else
 #include "tachyon/math/finite_fields/prime_field.h"
+#endif
 
 namespace tachyon::math {
 
 class TACHYON_EXPORT GoldilocksConfig {
  public:
+#if defined(TACHYON_GOLDILOCKS_BACKEND)
   constexpr static bool kIsGoldilocks = true;
+#else
+  constexpr static bool kIsSpecialPrime = false;
+#endif
 
   constexpr static size_t kModulusBits = 64;
   // 2^64 - 2^32 + 1
@@ -16,6 +23,7 @@ class TACHYON_EXPORT GoldilocksConfig {
   // Hex: 0xffffffff00000001
   constexpr static BigInt<1> kModulus =
       BigInt<1>(UINT64_C(18446744069414584321));
+  constexpr static BigInt<1> kOne = BigInt<1>(UINT64_C(4294967295));
 
   constexpr static uint64_t ExtensionDegree() { return 1; }
 
