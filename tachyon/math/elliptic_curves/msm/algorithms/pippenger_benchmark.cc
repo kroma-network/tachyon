3e1b99619618d940251bb0cbcea0f674da88fba5
Wonyong Kim(Ryan Kim) <ryan@lightscale.io>
2023-08-30 16:51:09 +0900

    feat(math): revert memory cache in msm
    This doesn't improve a performance that much. It just occupies memory.
    So I removed the feature that is introduced at 3381877.

--- tachyon/math/elliptic_curves/msm/algorithms/pippenger_benchmark.cc
+++ tachyon/math/elliptic_curves/msm/algorithms/pippenger_benchmark.cc
@@ -5,7 +5,7 @@
 
 namespace tachyon::math {
 
-template <typename PointTy, bool IsRandom, bool ClearCache>
+template <typename PointTy, bool IsRandom>
 void BM_Pippenger(benchmark::State& state) {
   PointTy::Curve::Init();
   MSMTestSet<PointTy> test_set;
@@ -16,7 +16,6 @@ void BM_Pippenger(benchmark::State& state) {
         MSMTestSet<PointTy>::NonUniform(state.range(0), 10, MSMMethod::kNone);
   }
   Pippenger<PointTy> pippenger;
-  pippenger.SetClearCacheForTesting(ClearCache);
   using ReturnTy = typename Pippenger<PointTy>::ReturnTy;
   ReturnTy ret;
   for (auto _ : state) {
@@ -27,35 +26,19 @@ void BM_Pippenger(benchmark::State& state) {
 }
 
 template <typename PointTy>
-void BM_PippengerRandomWithCache(benchmark::State& state) {
-  BM_Pippenger<PointTy, true, false>(state);
-}
-
-template <typename PointTy>
 void BM_PippengerRandom(benchmark::State& state) {
-  BM_Pippenger<PointTy, true, true>(state);
+  BM_Pippenger<PointTy, true>(state);
 }
 
 template <typename PointTy>
 void BM_PippengerNonUniform(benchmark::State& state) {
-  BM_Pippenger<PointTy, false, false>(state);
-}
-
-template <typename PointTy>
-void BM_PippengerNonUniformWithCache(benchmark::State& state) {
-  BM_Pippenger<PointTy, false, true>(state);
+  BM_Pippenger<PointTy, false>(state);
 }
 
 BENCHMARK_TEMPLATE(BM_PippengerRandom, bn254::G1AffinePoint)
     ->RangeMultiplier(2)
     ->Range(1 << 15, 1 << 20);
-BENCHMARK_TEMPLATE(BM_PippengerRandomWithCache, bn254::G1AffinePoint)
-    ->RangeMultiplier(2)
-    ->Range(1 << 15, 1 << 20);
 BENCHMARK_TEMPLATE(BM_PippengerNonUniform, bn254::G1AffinePoint)
-    ->RangeMultiplier(2)
-    ->Range(1 << 15, 1 << 20);
-BENCHMARK_TEMPLATE(BM_PippengerNonUniformWithCache, bn254::G1AffinePoint)
     ->RangeMultiplier(2)
     ->Range(1 << 15, 1 << 20);
 
@@ -82,22 +65,10 @@ BENCHMARK_TEMPLATE(BM_PippengerNonUniformWithCache, bn254::G1AffinePoint)
 // BM_PippengerRandom<bn254::G1AffinePoint>/262144               1928324699 ns   1928262578 ns            1
 // BM_PippengerRandom<bn254::G1AffinePoint>/524288               3531723976 ns   3528962397 ns            1
 // BM_PippengerRandom<bn254::G1AffinePoint>/1048576              6836301804 ns   6835938130 ns            1
-// BM_PippengerRandomWithCache<bn254::G1AffinePoint>/32768        285346270 ns    285329861 ns            2
-// BM_PippengerRandomWithCache<bn254::G1AffinePoint>/65536        556808710 ns    556777027 ns            1
-// BM_PippengerRandomWithCache<bn254::G1AffinePoint>/131072      1006542206 ns   1006495662 ns            1
-// BM_PippengerRandomWithCache<bn254::G1AffinePoint>/262144      1923613548 ns   1923518538 ns            1
-// BM_PippengerRandomWithCache<bn254::G1AffinePoint>/524288      3514778852 ns   3514655273 ns            1
-// BM_PippengerRandomWithCache<bn254::G1AffinePoint>/1048576     6841082573 ns   6840623047 ns            1
 // BM_PippengerNonUniform<bn254::G1AffinePoint>/32768             277929703 ns    277919829 ns            3
 // BM_PippengerNonUniform<bn254::G1AffinePoint>/65536             505287886 ns    505261371 ns            1
 // BM_PippengerNonUniform<bn254::G1AffinePoint>/131072            987757206 ns    987699281 ns            1
 // BM_PippengerNonUniform<bn254::G1AffinePoint>/262144           1875749111 ns   1875631566 ns            1
 // BM_PippengerNonUniform<bn254::G1AffinePoint>/524288           3408781767 ns   3408557514 ns            1
 // BM_PippengerNonUniform<bn254::G1AffinePoint>/1048576          6684160471 ns   6683772112 ns            1
-// BM_PippengerNonUniformWithCache<bn254::G1AffinePoint>/32768    277943452 ns    277933102 ns            3
-// BM_PippengerNonUniformWithCache<bn254::G1AffinePoint>/65536    509582043 ns    509548690 ns            1
-// BM_PippengerNonUniformWithCache<bn254::G1AffinePoint>/131072  1011629820 ns   1011588293 ns            1
-// BM_PippengerNonUniformWithCache<bn254::G1AffinePoint>/262144  1912229538 ns   1912126362 ns            1
-// BM_PippengerNonUniformWithCache<bn254::G1AffinePoint>/524288  3389491558 ns   3389264901 ns            1
-// BM_PippengerNonUniformWithCache<bn254::G1AffinePoint>/1048576 6666882277 ns   6666451543 ns            1
 // clang-format on
