05bd1ecec510e7ae38f65af81dfa7b422fe2fc8d
Wonyong Kim(Ryan Kim) <ryan@lightscale.io>
2023-09-27 16:35:12 +0900

    refac(math): constrain more on operator* between `ScalarField` and `Point`

--- tachyon/math/elliptic_curves/projective_point.h
+++ tachyon/math/elliptic_curves/projective_point.h
@@ -17,7 +17,9 @@ std::ostream& operator<<(std::ostream& os,
   return os << point.ToString();
 }
 
-template <typename Curve, typename ScalarField = typename Curve::ScalarField>
+template <typename ScalarField, typename Curve,
+          std::enable_if_t<std::is_same_v<
+              ScalarField, typename Curve::ScalarField>>* = nullptr>
 ProjectivePoint<Curve> operator*(const ScalarField& v,
                                  const ProjectivePoint<Curve>& point) {
   return point * v;
