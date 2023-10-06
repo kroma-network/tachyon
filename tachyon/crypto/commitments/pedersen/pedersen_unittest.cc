16ed8e60e7565ad774c2af4c3c622e62f9480289
Leegwangwoon <leegwangwoon@naver.com>
2023-10-06 17:12:11 +0900

    chore(crypto): fix namespace from math to crypto

--- tachyon/crypto/commitments/pedersen/pedersen_unittest.cc
+++ tachyon/crypto/commitments/pedersen/pedersen_unittest.cc
@@ -4,13 +4,13 @@
 
 #include "tachyon/math/elliptic_curves/bn/bn254/g1.h"
 
-namespace tachyon::math {
+namespace tachyon::crypto {
 
 namespace {
 
 class PedersenTest : public testing::Test {
  public:
-  static void SetUpTestSuite() { bn254::G1Curve::Init(); }
+  static void SetUpTestSuite() { math::bn254::G1Curve::Init(); }
 };
 
 }  // namespace
@@ -18,21 +18,21 @@ class PedersenTest : public testing::Test {
 TEST_F(PedersenTest, CommitPedersen) {
   const size_t max_size = 3;
 
-  PedersenParams<bn254::G1JacobianPoint> params =
-      PedersenParams<bn254::G1JacobianPoint>::Random(max_size);
+  PedersenParams<math::bn254::G1JacobianPoint> params =
+      PedersenParams<math::bn254::G1JacobianPoint>::Random(max_size);
 
-  std::vector<bn254::Fr> v =
-      base::CreateVector(max_size, []() { return bn254::Fr::Random(); });
+  std::vector<math::bn254::Fr> v =
+      base::CreateVector(max_size, []() { return math::bn254::Fr::Random(); });
 
-  bn254::Fr r = bn254::Fr::Random();
-  bn254::G1JacobianPoint commitment;
+  math::bn254::Fr r = math::bn254::Fr::Random();
+  math::bn254::G1JacobianPoint commitment;
   ASSERT_TRUE(params.Commit(v, r, &commitment));
 
-  VariableBaseMSM<bn254::G1JacobianPoint> msm;
-  bn254::G1JacobianPoint msm_result;
+  math::VariableBaseMSM<math::bn254::G1JacobianPoint> msm;
+  math::bn254::G1JacobianPoint msm_result;
   ASSERT_TRUE(msm.Run(params.generators(), v, &msm_result));
 
   EXPECT_EQ(commitment, msm_result + r * params.h());
 }
 
-}  // namespace tachyon::math
+}  // namespace tachyon::crypto
