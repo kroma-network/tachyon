669379daf83c8b85664348b195241d4d41a436cd
Wonyong Kim <chokobole33@gmail.com>
2023-10-09 23:52:12 +0900

    feat(math): implement batch inverse

--- tachyon/base/containers/adapters.h
+++ tachyon/base/containers/adapters.h
@@ -7,8 +7,15 @@
 
 #include <stddef.h>
 
+#include <algorithm>
 #include <iterator>
+#include <tuple>
 #include <utility>
+
+#include "absl/types/span.h"
+
+#include "tachyon/base/numerics/checked_math.h"
+#include "tachyon/base/template_util.h"
 
 namespace tachyon::base {
 namespace internal {
@@ -31,6 +38,174 @@ class ReversedAdapter {
   T& t_;
 };
 
+template <typename T>
+class ChunkedAdapter {
+ public:
+  class Iterator {
+   public:
+    using ItType = decltype(std::begin(std::declval<T&>()));
+
+    using underlying_value_type =
+        std::conditional_t<std::is_const_v<T>, const iter_value_t<ItType>,
+                           iter_value_t<ItType>>;
+
+    using difference_type = std::ptrdiff_t;
+    using value_type = absl::Span<underlying_value_type>;
+    using pointer = value_type*;
+    using reference = value_type&;
+    using iterator_category = std::forward_iterator_tag;
+
+    static Iterator begin(T& t, size_t chunk_size) {
+      return Iterator(std::begin(t), chunk_size,
+                      std::distance(std::begin(t), std::end(t)));
+    }
+    static Iterator end(T& t) { return Iterator(std::end(t)); }
+
+    Iterator(const Iterator& other) = default;
+    Iterator& operator=(const Iterator& other) = default;
+
+    bool operator==(const Iterator& other) const { return it_ == other.it_; }
+    bool operator!=(const Iterator& other) const { return !(*this == other); }
+
+    Iterator& operator++() {
+      it_ += len_;
+      offset_ += len_;
+      base::CheckedNumeric<size_t> len = len_;
+      len += chunk_size_;
+      size_t new_len = len.ValueOrDie();
+      if (new_len > size_) {
+        len_ = size_ - offset_;
+      } else {
+        len_ = chunk_size_;
+      }
+      value_ = value_type(&*it_, len_);
+      return *this;
+    }
+
+    Iterator operator++(int) {
+      Iterator it(*this);
+      ++(*this);
+      return it;
+    }
+
+    // NOTE(chokobole): To suppress -Werror,-Wignored-reference-qualifiers on
+    // mac
+    const std::remove_const_t<pointer> operator->() const { return &value_; }
+    pointer operator->() { return &value_; }
+
+    // NOTE(chokobole): To suppress -Werror,-Wignored-reference-qualifiers on
+    // mac
+    const std::remove_const_t<reference> operator*() const { return value_; }
+    reference operator*() { return value_; }
+
+   private:
+    explicit Iterator(ItType it) : it_(it), chunk_size_(0), size_(0), len_(0) {}
+    Iterator(ItType it, size_t chunk_size, size_t size)
+        : it_(it),
+          chunk_size_(chunk_size),
+          size_(size),
+          len_(std::min(chunk_size, size)),
+          value_(value_type(&*it, len_)) {}
+
+    ItType it_;
+    size_t offset_ = 0;
+    size_t chunk_size_;
+    size_t size_;
+    size_t len_;
+    value_type value_;
+  };
+
+  ChunkedAdapter(T& t, size_t chunk_size) : t_(t), chunk_size_(chunk_size) {}
+  ChunkedAdapter(const ChunkedAdapter& ca) : t_(ca.t_) {}
+
+  ChunkedAdapter& operator=(const ChunkedAdapter& ca) = delete;
+
+  Iterator begin() const { return Iterator::begin(t_, chunk_size_); }
+  Iterator end() const { return Iterator::end(t_); }
+
+ private:
+  T& t_;
+  size_t chunk_size_;
+};
+
+template <typename T, typename U>
+class ZippedAdapter {
+ public:
+  class Iterator {
+   public:
+    using ItType = decltype(std::begin(std::declval<T&>()));
+    using ItType2 = decltype(std::begin(std::declval<U&>()));
+
+    using underlying_value_type = iter_value_t<ItType>;
+    using underlying_value_type2 = iter_value_t<ItType2>;
+
+    using difference_type = std::ptrdiff_t;
+    using value_type =
+        std::tuple<underlying_value_type, underlying_value_type2>;
+    using pointer = value_type*;
+    using reference = value_type&;
+    using iterator_category = std::forward_iterator_tag;
+
+    static Iterator begin(T& t, U& u) {
+      return Iterator(std::begin(t), std::begin(u));
+    }
+    static Iterator end(T& t, U& u) {
+      return Iterator(std::end(t), std::end(u));
+    }
+
+    Iterator(const Iterator& other) = default;
+    Iterator& operator=(const Iterator& other) = default;
+
+    bool operator==(const Iterator& other) const {
+      return it_ == other.it_ && it2_ == other.it2_;
+    }
+    bool operator!=(const Iterator& other) const { return !(*this == other); }
+
+    Iterator& operator++() {
+      ++it_;
+      ++it2_;
+      value_ = value_type(*it_, *it2_);
+      return *this;
+    }
+
+    Iterator operator++(int) {
+      Iterator it(*this);
+      ++(*this);
+      return it;
+    }
+
+    // NOTE(chokobole): To suppress -Werror,-Wignored-reference-qualifiers on
+    // mac
+    const std::remove_const_t<pointer> operator->() const { return &value_; }
+    pointer operator->() { return &value_; }
+
+    // NOTE(chokobole): To suppress -Werror,-Wignored-reference-qualifiers on
+    // mac
+    const std::remove_const_t<reference> operator*() const { return value_; }
+    reference operator*() { return value_; }
+
+   private:
+    Iterator(ItType it, ItType2 it2)
+        : it_(it), it2_(it2), value_(value_type(*it, *it2)) {}
+
+    ItType it_;
+    ItType2 it2_;
+    value_type value_;
+  };
+
+  ZippedAdapter(T& t, U& u) : t_(t), u_(u) {}
+  ZippedAdapter(const ZippedAdapter& za) : t_(za.t_), u_(za.u_) {}
+
+  ZippedAdapter& operator=(const ZippedAdapter& za) = delete;
+
+  Iterator begin() const { return Iterator::begin(t_, u_); }
+  Iterator end() const { return Iterator::end(t_, u_); }
+
+ private:
+  T& t_;
+  U& u_;
+};
+
 }  // namespace internal
 
 // Reversed returns a container adapter usable in a range-based "for" statement
@@ -38,13 +213,47 @@ class ReversedAdapter {
 //
 // Example:
 //
-//   std::vector<int> v = ...;
+//   std::vector<int> v = {1, 2, 3, 4, 5};
 //   for (int i : tachyon::base::Reversed(v)) {
 //     // iterates through v from back to front
+//     // 5, 4, 3, 2, 1
 //   }
 template <typename T>
 internal::ReversedAdapter<T> Reversed(T& t) {
   return internal::ReversedAdapter<T>(t);
+}
+
+// Chunked returns a container adapter that can be used in a range-based "for"
+// loop to iterate over a container in chunks of a specified size.
+//
+// Example:
+//
+//   std::vector<int> v = {1, 2, 3, 4, 5};
+//   for (const absl::Span<int>& i : tachyon::base::Chunked(v, 2)) {
+//     // iterates through v in chunks
+//     // {1, 2}, {3, 4}, {5}
+//   }
+template <typename T>
+internal::ChunkedAdapter<T> Chunked(T& t, size_t chunk_size) {
+  return internal::ChunkedAdapter<T>(t, chunk_size);
+}
+
+// Zipped returns a container adapter that can be used in a range-based "for"
+// loop to iterate over 2 containers.
+// Note that in order to iterate safely, both lengths of the containers should
+// be same.
+//
+// Example:
+//
+//   std::vector<int> v = {1, 2, 3};
+//   std::vector<int> w = {4, 5, 6};
+//   for (const std::tuple<int, int>& i : tachyon::base::Zipped(v, w)) {
+//     // iterates through v in tuple
+//     // {1, 4}, {2, 5}, {3, 6}
+//   }
+template <typename T, typename U>
+internal::ZippedAdapter<T, U> Zipped(T& t, U& u) {
+  return internal::ZippedAdapter<T, U>(t, u);
 }
 
 }  // namespace tachyon::base
---
+++ tachyon/math/base/batch_inverse_benchmark.cc
@@ -0,0 +1,47 @@
+#include "benchmark/benchmark.h"
+
+#include "tachyon/base/containers/container_util.h"
+#include "tachyon/math/elliptic_curves/bn/bn254/fq.h"
+
+namespace tachyon::math {
+
+template <typename F>
+void BM_BatchInverse(benchmark::State& state) {
+  using BigIntTy = typename F::BigIntTy;
+
+  std::vector<F> fields = base::CreateVector(
+      state.range(0), [](size_t i) { return F::FromBigInt(BigIntTy(i)); });
+  for (auto _ : state) {
+    F::BatchInverseInPlace(fields);
+  }
+  benchmark::DoNotOptimize(fields);
+}
+
+BENCHMARK_TEMPLATE(BM_BatchInverse, bn254::Fq)
+    ->RangeMultiplier(2)
+    ->Range(1 << 15, 1 << 20);
+
+}  // namespace tachyon::math
+
+// clang-format off
+// Executing tests from //tachyon/math/base:batch_inverse_benchmark
+// -----------------------------------------------------------------------------
+// 2023-10-11T12:00:27+00:00
+// Running /home/ryan/.cache/bazel/_bazel_ryan/2e01f4ccafa60589f9bbdbefc5d15e2a/execroot/kroma_network_tachyon/bazel-out/k8-opt/bin/tachyon/math/base/batch_inverse_benchmark.runfiles/kroma_network_tachyon/tachyon/math/base/batch_inverse_benchmark
+// Run on (32 X 5500 MHz CPU s)
+// CPU Caches:
+//   L1 Data 48 KiB (x16)
+//   L1 Instruction 32 KiB (x16)
+//   L2 Unified 2048 KiB (x16)
+//   L3 Unified 36864 KiB (x1)
+// Load Average: 5.03, 2.47, 1.98
+// -----------------------------------------------------------------------------
+// Benchmark                                   Time             CPU   Iterations
+// -----------------------------------------------------------------------------
+// BM_BatchInverse<bn254::Fq>/32768       171015 ns       170921 ns         4026
+// BM_BatchInverse<bn254::Fq>/65536       496951 ns       477518 ns         1814
+// BM_BatchInverse<bn254::Fq>/131072     1048433 ns      1014639 ns          689
+// BM_BatchInverse<bn254::Fq>/262144     2118436 ns      2118300 ns          323
+// BM_BatchInverse<bn254::Fq>/524288     4223493 ns      4220959 ns          222
+// BM_BatchInverse<bn254::Fq>/1048576    7400200 ns      7321782 ns          125
+// clang-format on
