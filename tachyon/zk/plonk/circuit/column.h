993d4b8d99d364340ed8b8b8d4a8ea3b8a3f775a
Ryan Kim <ryan@lightscale.io>
2023-10-23 20:33:20 +0900

    feat(zk): implement `ColumnData`
    See
    [Column](https://github.com/kroma-network/halo2/blob/7d0a36990452c8e7ebd600de258420781a9b7917/halo2_proofs/src/dev/metadata.rs#L9-L16).

--- tachyon/zk/plonk/circuit/column.h
+++ tachyon/zk/plonk/circuit/column.h
@@ -20,6 +20,34 @@
 
 namespace tachyon::zk {
 
+class TACHYON_EXPORT ColumnData {
+ public:
+  ColumnData() = default;
+  ColumnData(ColumnType type, size_t index) : type_(type), index_(index) {}
+
+  ColumnType type() const { return type_; }
+  size_t index() const { return index_; }
+
+  bool operator==(const ColumnData& other) const {
+    return type_ == other.type_ && index_ == other.index_;
+  }
+  bool operator!=(const ColumnData& other) const { return !operator==(other); }
+
+  std::string ToString() const {
+    return absl::Substitute("{type: $0, index: $1}", ColumnTypeToString(type_),
+                            index_);
+  }
+
+ protected:
+  ColumnType type_ = ColumnType::kAny;
+  size_t index_ = 0;
+};
+
+template <typename H>
+H AbslHashValue(H h, const ColumnData& column) {
+  return H::combine(std::move(h), column.type(), column.index());
+}
+
 // AnyColumn c = FixedColumn(1); -> ok
 // AnyColumn c = AnyColumn(1); -> ok
 // FixedColumn c = FixedColumn(1); -> ok
@@ -27,12 +55,12 @@ namespace tachyon::zk {
 // FixedColumn c = AnyColumn(1); -> yes
 // See column_unittest.cc
 template <ColumnType C>
-class Column {
+class Column : public ColumnData {
  public:
   constexpr static ColumnType kDefaultType = C;
 
-  Column() = default;
-  explicit Column(size_t index) : index_(index) {}
+  Column() : ColumnData(C, 0) {}
+  explicit Column(size_t index) : ColumnData(C, index) {}
 
   // NOTE(chokobole): AdviceColumn can be constructed with an additional
   // argument |phase|.
@@ -40,7 +68,7 @@ class Column {
   //   AdviceColumn column(1, kSecondPhase);
   template <ColumnType C2 = C,
             std::enable_if_t<C2 == ColumnType::kAdvice>* = nullptr>
-  Column(size_t index, Phase phase) : index_(index), phase_(phase) {}
+  Column(size_t index, Phase phase) : ColumnData(C, index), phase_(phase) {}
 
   // NOTE(chokobole): in this case, |type_| is changed!
   //
@@ -53,7 +81,7 @@ class Column {
   template <ColumnType C2,
             std::enable_if_t<C == ColumnType::kAny && C != C2>* = nullptr>
   Column(const Column<C2>& other)
-      : type_(other.type_), index_(other.index_), phase_(other.phase_) {}
+      : ColumnData(other.type_, other.index_), phase_(other.phase_) {}
 
   // NOTE(chokobole): in this case, |type_| is not changed!
   //
@@ -67,7 +95,7 @@ class Column {
   template <ColumnType C2, std::enable_if_t<C != ColumnType::kAny &&
                                             C2 == ColumnType::kAny>* = nullptr>
   Column(const Column<C2>& other)
-      : index_(other.index_), phase_(other.phase_) {}
+      : ColumnData(C, other.index_), phase_(other.phase_) {}
 
   // FixedColumn c(FixedColumn(1));
   Column(const Column& other) = default;
@@ -85,8 +113,7 @@ class Column {
   template <ColumnType C2,
             std::enable_if_t<C == ColumnType::kAny && C != C2>* = nullptr>
   Column& operator=(const Column<C2>& other) {
-    type_ = other.type_;
-    index_ = other.index_;
+    ColumnData::operator=(other);
     phase_ = other.phase_;
     return *this;
   }
@@ -118,12 +145,11 @@ class Column {
   Phase phase() const { return phase_; }
 
   bool operator==(const Column& other) const {
+    if (!ColumnData::operator==(other)) return false;
     if (type_ == ColumnType::kAdvice) {
-      return type_ == other.type_ && index_ == other.index_ &&
-             phase_ == other.phase_;
-    } else {
-      return type_ == other.type_ && index_ == other.index_;
+      return phase_ == other.phase_;
     }
+    return true;
   }
   bool operator!=(const Column& other) const { return !operator==(other); }
 
@@ -145,8 +171,6 @@ class Column {
   template <ColumnType C2>
   friend class Column;
 
-  ColumnType type_ = C;
-  size_t index_ = 0;
   Phase phase_;
 };
 
