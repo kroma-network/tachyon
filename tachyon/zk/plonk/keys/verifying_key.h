b81b3e12cf4e4788323e8fce9e2dd81fbfd83495
dongchangYoo <yoodc8167@gmail.com>
2023-10-27 22:10:05 +0900

    feat(zk): implement `ProvingKey` and `VerifyingKey` w/o logic
    See
    [ProvingKey](https://github.com/kroma-network/halo2/blob/7d0a36990452c8e7ebd600de258420781a9b7917/halo2_proofs/src/plonk.rs#L267-L276),
    [VerifyingKey](https://github.com/kroma-network/halo2/blob/7d0a36990452c8e7ebd600de258420781a9b7917/halo2_proofs/src/plonk.rs#L50-L60).

--- tachyon/zk/plonk/keys/verifying_key.h
+++ tachyon/zk/plonk/keys/verifying_key.h
@@ -4,27 +4,58 @@
 #include <stddef.h>
 
 #include <memory>
+#include <utility>
 #include <vector>
 
-#include "tachyon/math/elliptic_curves/affine_point.h"
 #include "tachyon/math/polynomials/univariate/univariate_evaluation_domain.h"
+#include "tachyon/math/polynomials/univariate/univariate_evaluations.h"
+#include "tachyon/math/polynomials/univariate/univariate_polynomial.h"
+#include "tachyon/zk/plonk/commitment.h"
 #include "tachyon/zk/plonk/constraint_system.h"
+#include "tachyon/zk/plonk/permutation/permutation_verifying_key.h"
 
 namespace tachyon::zk {
 
 template <typename Curve, size_t MaxDegree>
-struct VerifyingKey {
-  using ScalarField = typename math::AffinePoint<Curve>::ScalarField;
+class VerifyingKey {
+ public:
+  using F = typename Curve::ScalarField;
 
-  std::unique_ptr<math::UnivariateEvaluationDomain<ScalarField, MaxDegree>>
-      domain;
-  std::vector<math::AffinePoint<Curve>> fixed_commitments;
-  ConstraintSystem<ScalarField> cs;
-  // Cached maximum degree of |cs| (which doesn't change after
-  // construction).
-  size_t cs_degree;
+  VerifyingKey(
+      std::unique_ptr<math::UnivariateEvaluationDomain<F, MaxDegree>> domain,
+      Commitments<Curve> fixed_commitments,
+      PermutationVerifyingKey<Curve> permutation_verifying_key,
+      ConstraintSystem<F> constraint_system)
+      : domain_(std::move(domain)),
+        fixed_commitments_(std::move(fixed_commitments)),
+        permutation_verifying_Key_(std::move(permutation_verifying_key)),
+        constraint_system_(std::move(constraint_system)) {}
+
+  const math::UnivariateEvaluationDomain<F, MaxDegree>* domain() const {
+    return domain_.get();
+  }
+
+  const Commitments<Curve>& fixed_commitments() const {
+    return fixed_commitments_;
+  }
+
+  const PermutationVerifyingKey<Curve>& permutation_verifying_key() const {
+    return permutation_verifying_Key_;
+  }
+
+  const ConstraintSystem<F>& constraint_system() const {
+    return constraint_system_;
+  }
+
+  const F& transcript_repr() const { return transcript_repr_; }
+
+ private:
+  std::unique_ptr<math::UnivariateEvaluationDomain<F, MaxDegree>> domain_;
+  Commitments<Curve> fixed_commitments_;
+  PermutationVerifyingKey<Curve> permutation_verifying_Key_;
+  ConstraintSystem<F> constraint_system_;
   // The representative of this |VerifyingKey| in transcripts.
-  ScalarField transcript_repr;
+  F transcript_repr_ = F::Zero();
 };
 
 }  // namespace tachyon::zk
