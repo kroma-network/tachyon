fd8e8a939719b1907706aef64a8517d16f5bd2fd
Ryan Kim <ryan@lightscale.io>
2023-11-01 23:46:56 +0900

    feat(zk): implement `ValueSource`
    See
    [ValueSource](https://github.com/kroma-network/halo2/blob/7d0a36990452c8e7ebd600de258420781a9b7917/halo2_proofs/src/plonk/evaluation.rs#L36-L61).

---
+++ tachyon/zk/plonk/circuit/expressions/evaluator/value_source_data.h
@@ -0,0 +1,36 @@
+// Copyright 2020-2022 The Electric Coin Company
+// Copyright 2022 The Halo2 developers
+// Use of this source code is governed by a MIT/Apache-2.0 style license that
+// can be found in the LICENSE-MIT.halo2 and the LICENCE-APACHE.halo2
+// file.
+
+#ifndef TACHYON_ZK_PLONK_CIRCUIT_EXPRESSIONS_EVALUATOR_VALUE_SOURCE_DATA_H_
+#define TACHYON_ZK_PLONK_CIRCUIT_EXPRESSIONS_EVALUATOR_VALUE_SOURCE_DATA_H_
+
+#include <stddef.h>
+
+#include <vector>
+
+namespace tachyon::zk {
+
+template <typename Poly>
+struct ValueSourceData {
+  using F = typename Poly::Field;
+
+  std::vector<size_t> rotations;
+  std::vector<F> constants;
+  std::vector<F> intermediates;
+  std::vector<Poly> fixeds;
+  std::vector<Poly> advices;
+  std::vector<Poly> instances;
+  std::vector<F> challenges;
+  F beta;
+  F gamma;
+  F theta;
+  F y;
+  F previous_value;
+};
+
+}  // namespace tachyon::zk
+
+#endif  // TACHYON_ZK_PLONK_CIRCUIT_EXPRESSIONS_EVALUATOR_VALUE_SOURCE_DATA_H_
---
+++ tachyon/zk/plonk/circuit/expressions/evaluator/value_source_unittest.cc
@@ -0,0 +1,48 @@
+#include "tachyon/zk/plonk/circuit/expressions/evaluator/value_source.h"
+
+#include "gtest/gtest.h"
+
+#include "tachyon/base/random.h"
+
+namespace tachyon::zk {
+
+#define TEST_CONSTANTS(name, index)                                       \
+  size_t value = base::Uniform(base::Range<size_t>::From(index + 1));     \
+                                                                          \
+  EXPECT_TRUE(ValueSource::Constant(index).Is##name##Constant());         \
+  EXPECT_FALSE(ValueSource::Constant(value).Is##name##Constant());        \
+  EXPECT_FALSE(ValueSource::Intermediate(index).Is##name##Constant());    \
+  EXPECT_FALSE(ValueSource::Fixed(index, index).Is##name##Constant());    \
+  EXPECT_FALSE(ValueSource::Advice(index, index).Is##name##Constant());   \
+  EXPECT_FALSE(ValueSource::Instance(index, index).Is##name##Constant()); \
+  EXPECT_FALSE(ValueSource::Challenge(index).Is##name##Constant());       \
+  EXPECT_FALSE(ValueSource::Beta().Is##name##Constant());                 \
+  EXPECT_FALSE(ValueSource::Gamma().Is##name##Constant());                \
+  EXPECT_FALSE(ValueSource::Theta().Is##name##Constant());                \
+  EXPECT_FALSE(ValueSource::Y().Is##name##Constant());                    \
+  EXPECT_FALSE(ValueSource::PreviousValue().Is##name##Constant())
+
+TEST(ValueSourceTest, IsZeroConstant) {
+  TEST_CONSTANTS(Zero, 0);
+  EXPECT_TRUE(ValueSource::ZeroConstant().IsZeroConstant());
+  EXPECT_FALSE(ValueSource::OneConstant().IsZeroConstant());
+  EXPECT_FALSE(ValueSource::TwoConstant().IsZeroConstant());
+}
+
+TEST(ValueSourceTest, IsOneConstant) {
+  TEST_CONSTANTS(One, 1);
+  EXPECT_FALSE(ValueSource::ZeroConstant().IsOneConstant());
+  EXPECT_TRUE(ValueSource::OneConstant().IsOneConstant());
+  EXPECT_FALSE(ValueSource::TwoConstant().IsOneConstant());
+}
+
+TEST(ValueSourceTest, IsTwoConstant) {
+  TEST_CONSTANTS(Two, 2);
+  EXPECT_FALSE(ValueSource::ZeroConstant().IsTwoConstant());
+  EXPECT_FALSE(ValueSource::OneConstant().IsTwoConstant());
+  EXPECT_TRUE(ValueSource::TwoConstant().IsTwoConstant());
+}
+
+#undef TEST_CONSTANTS
+
+}  // namespace tachyon::zk
