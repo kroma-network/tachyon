b59cb18a2c0612a39f4b2c69e113ee737ead866b
Ryan Kim <ryan@lightscale.io>
2023-11-03 18:12:44 +0900

    feat(zk): implement `BlindedPolynomial` and `BlindedPolynomialCommitment`

---
+++ tachyon/zk/base/blinded_polynomial.h
@@ -0,0 +1,35 @@
+// Copyright 2020-2022 The Electric Coin Company
+// Copyright 2022 The Halo2 developers
+// Use of this source code is governed by a MIT/Apache-2.0 style license that
+// can be found in the LICENSE-MIT.halo2 and the LICENCE-APACHE.halo2
+// file.
+
+#ifndef TACHYON_ZK_BASE_BLINDED_POLYNOMIAL_H_
+#define TACHYON_ZK_BASE_BLINDED_POLYNOMIAL_H_
+
+#include <utility>
+
+namespace tachyon::zk {
+
+template <typename Poly>
+class BlindedPolynomial {
+ public:
+  using F = typename Poly::Field;
+
+  BlindedPolynomial() = default;
+  BlindedPolynomial(const Poly& poly, const F& blind)
+      : poly_(poly), blind_(blind) {}
+  BlindedPolynomial(Poly&& poly, F&& blind)
+      : poly_(std::move(poly)), blind_(std::move(blind)) {}
+
+  const Poly& poly() const { return poly_; }
+  const F& blind() const { return blind_; }
+
+ private:
+  Poly poly_;
+  F blind_;
+};
+
+}  // namespace tachyon::zk
+
+#endif  // TACHYON_ZK_BASE_BLINDED_POLYNOMIAL_H_
