2e775a8d1fcf20720e1183df7629ddd74fc03ea3
Luke <luke@lightscale.io>
2023-11-07 23:04:05 +0900

    feat(zk): add move constructor on `ProverQuery` and `VerifierQuery`
    - add move constructors
    - add copyright comment

--- tachyon/zk/base/prover_query.h
+++ tachyon/zk/base/prover_query.h
@@ -1,5 +1,13 @@
+// Copyright 2020-2022 The Electric Coin Company
+// Copyright 2022 The Halo2 developers
+// Use of this source code is governed by a MIT/Apache-2.0 style license that
+// can be found in the LICENSE-MIT.halo2 and the LICENCE-APACHE.halo2
+// file.
+
 #ifndef TACHYON_ZK_BASE_PROVER_QUERY_H_
 #define TACHYON_ZK_BASE_PROVER_QUERY_H_
+
+#include <utility>
 
 #include "tachyon/zk/base/blinded_polynomial.h"
 
@@ -11,16 +19,19 @@ class ProverQuery {
   using F = typename PCSTy::Field;
   using Poly = typename PCSTy::Poly;
 
-  ProverQuery(Ref<const F> point, Ref<const BlindedPolynomial<Poly>> poly)
+  ProverQuery(const F& point, Ref<const BlindedPolynomial<Poly>> poly)
       : point_(point), poly_(poly) {}
 
-  Ref<const F> point() const { return point_; }
+  ProverQuery(F&& point, Ref<const BlindedPolynomial<Poly>> poly)
+      : point_(std::move(point)), poly_(poly) {}
+
+  const F& point() const { return point_; }
   Ref<const BlindedPolynomial<Poly>> poly() const { return poly_; }
 
   F Evaluate() const { return poly_->poly().Evaluate(*point_); }
 
  private:
-  Ref<const F> point_;
+  F point_;
   Ref<const BlindedPolynomial<Poly>> poly_;
 };
 
