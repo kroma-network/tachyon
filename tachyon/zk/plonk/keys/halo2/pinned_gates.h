47156f41216d752468f38a9da1c3621423fb739f
Ryan Kim <ryan@lightscale.io>
2023-11-23 11:15:08 +0900

    feat(zk): implement `PinnedGates`
    See
    [PinnedGates](https://github.com/kroma-network/halo2/blob/7d0a36990452c8e7ebd600de258420781a9b7917/halo2_proofs/src/plonk/circuit.rs#L1455-L1463)

---
+++ tachyon/zk/plonk/keys/halo2/pinned_gates.h
@@ -0,0 +1,56 @@
+// Copyright 2020-2022 The Electric Coin Company
+// Copyright 2022 The Halo2 developers
+// Use of this source code is governed by a MIT/Apache-2.0 style license that
+// can be found in the LICENSE-MIT.halo2 and the LICENCE-APACHE.halo2
+// file.
+
+#ifndef TACHYON_ZK_PLONK_KEYS_HALO2_PINNED_GATES_H_
+#define TACHYON_ZK_PLONK_KEYS_HALO2_PINNED_GATES_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "tachyon/zk/plonk/circuit/expressions/expression_stringifier.h"
+#include "tachyon/zk/plonk/circuit/gate.h"
+
+namespace tachyon {
+namespace zk::halo2 {
+
+template <typename F>
+class PinnedGates {
+ public:
+  explicit PinnedGates(const std::vector<Gate<F>>& gates) : gates_(gates) {}
+
+  const std::vector<Gate<F>>& gates() const { return gates_; }
+
+ private:
+  const std::vector<Gate<F>>& gates_;
+};
+
+}  // namespace zk::halo2
+
+namespace base::internal {
+
+template <typename F>
+class RustDebugStringifier<zk::halo2::PinnedGates<F>> {
+ public:
+  static std::ostream& AppendToStream(
+      std::ostream& os, RustFormatter& fmt,
+      const zk::halo2::PinnedGates<F>& pinned_gates) {
+    DebugList list = fmt.DebugList();
+    for (const zk::Gate<F>& gate : pinned_gates.gates()) {
+      const std::vector<std::unique_ptr<zk::Expression<F>>>& polys =
+          gate.polys();
+      for (const std::unique_ptr<zk::Expression<F>>& poly : polys) {
+        list.Entry(*poly);
+      }
+    }
+    return os << list.Finish();
+  }
+};
+
+}  // namespace base::internal
+}  // namespace tachyon
+
+#endif  // TACHYON_ZK_PLONK_KEYS_HALO2_PINNED_GATES_H_
