0c6e9fba3778466b36262fb301baa7626f5bc99e
Ryan Kim <ryan@lightscale.io>
2023-11-23 13:54:50 +0900

    feat(zk): implement `PinnedEvaluationDomain`
    See
    [PinnedEvaluationDomain](https://github.com/kroma-network/halo2/blob/7d0a36990452c8e7ebd600de258420781a9b7917/halo2_proofs/src/poly/domain.rs#L593-L600).

---
+++ tachyon/zk/plonk/keys/halo2/pinned_evaluation_domain.h
@@ -0,0 +1,61 @@
+// Copyright 2020-2022 The Electric Coin Company
+// Copyright 2022 The Halo2 developers
+// Use of this source code is governed by a MIT/Apache-2.0 style license that
+// can be found in the LICENSE-MIT.halo2 and the LICENCE-APACHE.halo2
+// file.
+
+#ifndef TACHYON_ZK_PLONK_KEYS_HALO2_PINNED_EVALUATION_DOMAIN_H_
+#define TACHYON_ZK_PLONK_KEYS_HALO2_PINNED_EVALUATION_DOMAIN_H_
+
+#include <stdint.h>
+
+#include <string>
+#include <utility>
+
+#include "tachyon/base/strings/rust_stringifier.h"
+#include "tachyon/zk/base/field_stringifier.h"
+
+namespace tachyon {
+namespace zk::halo2 {
+
+template <typename F>
+class PinnedEvaluationDomain {
+ public:
+  PinnedEvaluationDomain() = default;
+  PinnedEvaluationDomain(uint32_t k, uint32_t extended_k, const F& omega)
+      : k_(k), extended_k_(extended_k), omega_(omega) {}
+  PinnedEvaluationDomain(uint32_t k, uint32_t extended_k, F&& omega)
+      : k_(k), extended_k_(extended_k), omega_(std::move(omega)) {}
+
+  uint32_t k() const { return k_; }
+  uint32_t extended_k() const { return extended_k_; }
+  const F& omega() const { return omega_; }
+
+ private:
+  uint32_t k_ = 0;
+  uint32_t extended_k_ = 0;
+  F omega_;
+};
+
+}  // namespace zk::halo2
+
+namespace base::internal {
+
+template <typename F>
+class RustDebugStringifier<zk::halo2::PinnedEvaluationDomain<F>> {
+ public:
+  static std::ostream& AppendToStream(
+      std::ostream& os, RustFormatter& fmt,
+      const zk::halo2::PinnedEvaluationDomain<F>& value) {
+    return os << fmt.DebugStruct("PinnedEvaluationDomain")
+                     .Field("k", value.k())
+                     .Field("extended_k", value.extended_k())
+                     .Field("omega", value.omega())
+                     .Finish();
+  }
+};
+
+}  // namespace base::internal
+}  // namespace tachyon
+
+#endif  // TACHYON_ZK_PLONK_KEYS_HALO2_PINNED_EVALUATION_DOMAIN_H_
