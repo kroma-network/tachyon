c2dbd9025f9f0034680eb06478ec11ac63bdc571
dongchangYoo <yoodc8167@gmail.com>
2023-11-29 20:35:21 +0900

    feat(zk): implement `PermutationArgument::Commit()`
    See
    [PermutationArgument](https://github.com/kroma-network/halo2/blob/7d0a36990452c8e7ebd600de258420781a9b7917/halo2_proofs/src/plonk/permutation/prover.rs#L43-L188).

--- tachyon/zk/plonk/permutation/permutation_argument_unittest.cc
+++ tachyon/zk/plonk/permutation/permutation_argument_unittest.cc
@@ -10,34 +10,77 @@
 
 #include "tachyon/base/random.h"
 #include "tachyon/zk/base/halo2_prover_test.h"
+#include "tachyon/zk/plonk/permutation/permutation_assembly.h"
 
 namespace tachyon::zk {
 
-class PermutationArgumentTest : public Halo2ProverTest {};
+class PermutationArgumentTest : public Halo2ProverTest {
+ public:
+  void SetUp() override {
+    Halo2ProverTest::SetUp();
+
+    Evals cycled_column = Evals::Random();
+    fixed_columns_ = {cycled_column, Evals::Random(), Evals::Random()};
+    advice_columns_ = {Evals::Random(), cycled_column, Evals::Random()};
+    instance_columns_ = {cycled_column, Evals::Random(), Evals::Random()};
+
+    table_ = Table<Evals>(absl::MakeConstSpan(fixed_columns_),
+                          absl::MakeConstSpan(advice_columns_),
+                          absl::MakeConstSpan(instance_columns_));
+
+    column_keys_ = {
+        FixedColumnKey(0), AdviceColumnKey(0), InstanceColumnKey(0),
+        FixedColumnKey(1), AdviceColumnKey(1), InstanceColumnKey(1),
+        FixedColumnKey(2), AdviceColumnKey(2), InstanceColumnKey(2),
+    };
+    argument_ = PermutationArgument(column_keys_);
+
+    unpermuted_table_ = UnpermutedTable<Evals>::Construct(column_keys_.size(),
+                                                          prover_->domain());
+  }
+
+ protected:
+  std::vector<Evals> fixed_columns_;
+  std::vector<Evals> advice_columns_;
+  std::vector<Evals> instance_columns_;
+  Table<Evals> table_;
+
+  std::vector<AnyColumnKey> column_keys_;
+  PermutationArgument argument_;
+  UnpermutedTable<Evals> unpermuted_table_;
+};
 
 TEST_F(PermutationArgumentTest, AddColumn) {
-  std::vector<AnyColumnKey> columns = {FixedColumnKey(0),
-                                       AdviceColumnKey(1, kSecondPhase),
-                                       InstanceColumnKey(2)};
-  PermutationArgument argument(columns);
-  EXPECT_EQ(argument.columns(), columns);
-
   // Already included column should not be added.
-  AnyColumnKey col = base::UniformElement(columns);
-  argument.AddColumn(col);
-  EXPECT_EQ(argument.columns(), columns);
+  AnyColumnKey col = base::UniformElement(column_keys_);
+  argument_.AddColumn(col);
+  EXPECT_EQ(argument_.columns(), column_keys_);
 
   // Advice column whose phase is different should be added.
   AdviceColumnKey col2(1, Phase(3));
-  argument.AddColumn(col2);
-  columns.push_back(col2);
-  EXPECT_EQ(argument.columns(), columns);
+  argument_.AddColumn(col2);
+  column_keys_.push_back(col2);
+  EXPECT_EQ(argument_.columns(), column_keys_);
 
   // New Instance column should be added.
   InstanceColumnKey col3(3);
-  argument.AddColumn(col3);
-  columns.push_back(col3);
-  EXPECT_EQ(argument.columns(), columns);
+  argument_.AddColumn(col3);
+  column_keys_.push_back(col3);
+  EXPECT_EQ(argument_.columns(), column_keys_);
+}
+
+// TODO(chokobole): Implement test codes correctly. This just test compilation.
+TEST_F(PermutationArgumentTest, Commit) {
+  PermutationAssembly<PCS> assembly =
+      PermutationAssembly<PCS>::CreateForTesting(
+          column_keys_, CycleStore(column_keys_.size(), kDomainSize));
+
+  PermutationProvingKey<PCS> pk = assembly.BuildProvingKey(prover_->domain());
+
+  F beta = F::Random();
+  F gamma = F::Random();
+
+  argument_.Commit(prover_.get(), table_, prover_->pcs().N(), pk, beta, gamma);
 }
 
 }  // namespace tachyon::zk
