663a57a0220646bd3d9caa2c623fe1e49a93d5ba
dongchangYoo <yoodc8167@gmail.com>
2023-11-29 22:24:35 +0900

    feat(zk): implement `PermutationEvaluted::Open()`
    See
    [Evaluated<C>::open()](https://github.com/kroma-network/halo2/blob/7d0a36990452c8e7ebd600de258420781a9b7917/halo2_proofs/src/plonk/permutation/prover.rs#L278-L324).

--- tachyon/zk/plonk/permutation/permutation_evaluated.h
+++ tachyon/zk/plonk/permutation/permutation_evaluated.h
@@ -11,6 +11,9 @@
 #include <vector>
 
 #include "tachyon/zk/base/blinded_polynomial.h"
+#include "tachyon/zk/base/prover.h"
+#include "tachyon/zk/base/prover_query.h"
+#include "tachyon/zk/plonk/circuit/rotation.h"
 
 namespace tachyon::zk {
 
@@ -24,6 +27,29 @@ class PermutationEvaluated {
 
   const std::vector<BlindedPolynomial<Poly>>& product_polys() const {
     return product_polys_;
+  }
+
+  template <typename PCSTy, typename F>
+  std::vector<ProverQuery<PCSTy>> Open(const Prover<PCSTy>* prover,
+                                       const F& x) const {
+    std::vector<ProverQuery<PCSTy>> ret;
+    ret.reserve(product_polys_.size() * 3 - 1);
+
+    F x_next = Rotation::Next().RotateOmega(prover->domain(), x);
+    for (const BlindedPolynomial<Poly>& blinded_poly : product_polys_) {
+      ret.emplace_back(x, blinded_poly.ToRef());
+      ret.emplace_back(x_next, blinded_poly.ToRef());
+    }
+
+    int32_t blinding_factors =
+        static_cast<int32_t>(prover->blinder().blinding_factors());
+    F x_last =
+        Rotation(-(blinding_factors + 1)).RotateOmega(prover->domain(), x);
+    for (auto it = product_polys_.rbegin() + 1; it != product_polys_.rend();
+         ++it) {
+      ret.emplace_back(x_last, it->ToRef());
+    }
+    return ret;
   }
 
  private:
