79653d5388be151be091fec14211d53968dda0a9
Ryan Kim <ryan@lightscale.io>
2023-12-04 14:24:22 +0900

    feat(base): add `base::Map()` with a callback of 2 arguments with index
    This is needed by `SelectorCompressor<F>::Process()`.

--- tachyon/base/containers/container_util_unittest.cc
+++ tachyon/base/containers/container_util_unittest.cc
@@ -47,6 +47,17 @@ TEST(ContainerUtilTest, Map) {
               testing::ContainerEq(std::vector<double>{2.0, 4.0, 6.0}));
 }
 
+TEST(ContainerUtilTest, MapWithIdx) {
+  std::vector<int> arr({1, 2, 3});
+  EXPECT_THAT(
+      Map(arr.begin(), arr.end(),
+          [](size_t idx, int v) { return static_cast<double>(v * 2 + idx); }),
+      testing::ContainerEq(std::vector<double>{2.0, 5.0, 8.0}));
+  EXPECT_THAT(Map(arr, [](size_t idx,
+                          int v) { return static_cast<double>(v * 2 + idx); }),
+              testing::ContainerEq(std::vector<double>{2.0, 5.0, 8.0}));
+}
+
 TEST(ContainerUtilTest, FlatMap) {
   std::vector<int> arr({1, 2});
   arr = base::FlatMap(arr, [](int value) {
