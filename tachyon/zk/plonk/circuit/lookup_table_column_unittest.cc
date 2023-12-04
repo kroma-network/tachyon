6a82b0a696dfe2f0c5fae7eea03d0f8b793af5c0
Ryan Kim <ryan@lightscale.io>
2023-12-04 15:45:46 +0900

    fix(zk): add `AbslHashValue` to `LookupTableColumn`
    This is needed by `SingleChipLayouter::AssignLookupTable()`.

---
+++ tachyon/zk/plonk/circuit/lookup_table_column_unittest.cc
@@ -0,0 +1,14 @@
+#include "tachyon/zk/plonk/circuit/lookup_table_column.h"
+
+#include "absl/hash/hash_testing.h"
+#include "gtest/gtest.h"
+
+namespace tachyon::zk {
+
+TEST(LookupTableColumnTest, Hash) {
+  EXPECT_TRUE(absl::VerifyTypeImplementsAbslHashCorrectly(
+      std::make_tuple(LookupTableColumn(), LookupTableColumn(FixedColumnKey(1)),
+                      LookupTableColumn(FixedColumnKey(2)))));
+}
+
+}  // namespace tachyon::zk
