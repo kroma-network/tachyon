0070f1e9344a626db9d3e8b3e0a7b8a0e1a471f6
dongchangYoo <yoodc8167@gmail.com>
2023-12-06 17:14:08 +0900

    feat(zk): implement `Synthesizer`
    `Synthesizer` is a class that includes `AdviceSingle` and the logic to build it.
    See [AdviceSingle](https://github.com/kroma-network/halo2/blob/7d0a36990452c8e7ebd600de258420781a9b7917/halo2_proofs/src/plonk/prover.rs#L134-L138), [Logic](https://github.com/kroma-network/halo2/blob/7d0a36990452c8e7ebd600de258420781a9b7917/halo2_proofs/src/plonk/prover.rs#L284-L430)

---
+++ tachyon/zk/plonk/prover/synthesizer_unittest.cc
@@ -0,0 +1,55 @@
+// Copyright 2020-2022 The Electric Coin Company
+// Copyright 2022 The Halo2 developers
+// Use of this source code is governed by a MIT/Apache-2.0 style license that
+// can be found in the LICENSE-MIT.halo2 and the LICENCE-APACHE.halo2
+// file.
+
+#include "tachyon/zk/plonk/prover/synthesizer.h"
+
+#include <memory>
+
+#include "gtest/gtest.h"
+
+#include "tachyon/zk/base/halo2/halo2_prover_test.h"
+#include "tachyon/zk/plonk/circuit/examples/simple_circuit.h"
+#include "tachyon/zk/plonk/keys/halo2/pinned_verifying_key.h"
+
+namespace tachyon::zk {
+namespace {
+
+class SynthesizerTest : public Halo2ProverTest {
+ public:
+  using F = typename PCS::Field;
+
+  void SetUp() override {
+    Halo2ProverTest::SetUp();
+
+    circuits_ = {SimpleCircuit<F>(), SimpleCircuit<F>()};
+
+    CHECK(VerifyingKey<PCS>::Generate<SimpleCircuit<F>>(
+        prover_.get(), circuits_[0], &verifying_key_));
+
+    synthesizer_ =
+        Synthesizer<PCS>(circuits_.size(), &verifying_key_.constraint_system());
+  }
+
+ protected:
+  std::vector<SimpleCircuit<F>> circuits_;
+  VerifyingKey<PCS> verifying_key_;
+  Synthesizer<PCS> synthesizer_;
+};
+
+}  // namespace
+
+// TODO(dongchangYoo): it should be verified if it produces the expected values.
+TEST_F(SynthesizerTest, GenerateAdviceColumns) {
+  std::vector<std::vector<Evals>> instance_columns_vec = base::CreateVector(
+      circuits_.size(),
+      []() { return base::CreateVector(1, Evals::Random()); });
+  synthesizer_.GenerateAdviceColumns(prover_.get(), circuits_,
+                                     instance_columns_vec);
+
+  std::vector<F> challenges = synthesizer_.ExportChallenges();
+}
+
+}  // namespace tachyon::zk
