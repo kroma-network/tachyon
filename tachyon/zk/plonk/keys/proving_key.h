e684f63f0c1bef6e7961553158212c5d1415c934
Ryan Kim <ryan@lightscale.io>
2023-12-12 14:53:38 +0900

    refac(zk): rename `fixed_values` to `fixed_columns` for consistency with others

--- tachyon/zk/plonk/keys/proving_key.h
+++ tachyon/zk/plonk/keys/proving_key.h
@@ -25,13 +25,13 @@ class ProvingKey {
 
   ProvingKey() = default;
   ProvingKey(VerifyingKey<PCSTy>&& verifying_key, Poly&& l_first, Poly&& l_last,
-             Poly&& l_active_row, Evals&& fixed_values, Evals&& fixed_polys,
+             Poly&& l_active_row, Evals&& fixed_columns, Evals&& fixed_polys,
              PermutationProvingKey<Poly, Evals>&& permutation_proving_key)
       : verifying_key_(std::move(verifying_key)),
         l_first_(std::move(l_first)),
         l_last_(std::move(l_last)),
         l_active_row_(std::move(l_active_row)),
-        fixed_values_(std::move(fixed_values)),
+        fixed_columns_(std::move(fixed_columns)),
         fixed_polys_(std::move(fixed_polys)),
         permutation_proving_key_(std::move(permutation_proving_key)) {}
 
@@ -39,7 +39,7 @@ class ProvingKey {
   const Poly& l_first() const { return l_first_; }
   const Poly& l_last() const { return l_last_; }
   const Poly& l_active_row() const { return l_active_row_; }
-  const std::vector<Evals>& fixed_values() const { return fixed_values_; }
+  const std::vector<Evals>& fixed_columns() const { return fixed_columns_; }
   const std::vector<Poly>& fixed_polys() const { return fixed_polys_; }
   const PermutationProvingKey<Poly, Evals>& permutation_proving_key() const {
     return permutation_proving_key_;
@@ -50,7 +50,7 @@ class ProvingKey {
   Poly l_first_;
   Poly l_last_;
   Poly l_active_row_;
-  std::vector<Evals> fixed_values_;
+  std::vector<Evals> fixed_columns_;
   std::vector<Poly> fixed_polys_;
   PermutationProvingKey<Poly, Evals> permutation_proving_key_;
   VanishingArgument<PCSTy> vanishing_argument_;
