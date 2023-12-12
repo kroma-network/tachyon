c6ed4e0903ce2fc412be5a3fba61d304e4e857ce
TomTaehoonKim <ta@lightscale.io>
2023-12-12 22:34:32 +0900

    feat(zk): introduce `EntityTy`

---
+++ tachyon/zk/base/entities/entity_ty.h
@@ -0,0 +1,13 @@
+#ifndef TACHYON_ZK_BASE_ENTITIES_ENTITY_TY_H_
+#define TACHYON_ZK_BASE_ENTITIES_ENTITY_TY_H_
+
+namespace tachyon::zk {
+
+enum class EntityTy {
+  kProver,
+  kVerifier,
+};
+
+}  // namespace tachyon::zk
+
+#endif  // TACHYON_ZK_BASE_ENTITIES_ENTITY_TY_H_
