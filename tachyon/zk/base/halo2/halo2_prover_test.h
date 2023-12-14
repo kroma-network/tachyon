5d812c820cfbd2422bbe86bde3c7b033c3969b87
Ryan Kim <ryan@lightscale.io>
2023-12-14 14:46:11 +0900

    test(zk): change transcript to `Blake2b` for halo2 integration test
    If you look at circuit examples, most of examples uses
    `Blake2bTranscript`. `PoseidonTranscript` is only used for production.
    That's why this commit changes a transcript of `Halo2ProverTest` to
    `Blake2bTranscript`.

--- tachyon/zk/base/halo2/halo2_prover_test.h
+++ tachyon/zk/base/halo2/halo2_prover_test.h
@@ -7,11 +7,12 @@
 #include "absl/memory/memory.h"
 #include "gtest/gtest.h"
 
-#include "tachyon/crypto/transcripts/poseidon_transcript.h"
+#include "tachyon/crypto/transcripts/blake2b_transcript.h"
 #include "tachyon/math/elliptic_curves/bn/bn254/g1.h"
 #include "tachyon/math/elliptic_curves/bn/bn254/g2.h"
 #include "tachyon/math/polynomials/univariate/univariate_evaluation_domain_factory.h"
 #include "tachyon/zk/base/commitments/shplonk_extension.h"
+#include "tachyon/zk/base/halo2/halo2_constants.h"
 #include "tachyon/zk/base/halo2/halo2_prover.h"
 
 namespace tachyon::zk {
@@ -44,8 +45,8 @@ class Halo2ProverTest : public testing::Test {
     base::VectorBuffer write_buf;
     std::unique_ptr<crypto::TranscriptWriter<math::bn254::G1AffinePoint>>
         writer =
-            absl::WrapUnique(new crypto::PoseidonWriter<math::bn254::G1Curve>(
-                std::move(write_buf)));
+            absl::WrapUnique(new crypto::Blake2bWriter<math::bn254::G1Curve>(
+                std::move(write_buf), halo2::kTranscriptStr));
 
     prover_ = std::make_unique<Halo2Prover<PCS>>(
         Halo2Prover<PCS>::CreateFromRandomSeed(
