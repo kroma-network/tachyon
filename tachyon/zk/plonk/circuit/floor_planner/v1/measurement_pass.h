07a16f61c9e19b8e047e4798d9a80869900bf106
TomTaehoonKim <ta@lightscale.io>
2023-12-18 22:02:40 +0900

    feat(zk): implement `MeasurementPass`
    See
    [MeasurementPass](https://github.com/kroma-network/halo2/blob/7d0a36990452c8e7ebd600de258420781a9b7917/halo2_proofs/src/circuit/floor_planner/v1.rs#L239-L272)

---
+++ tachyon/zk/plonk/circuit/floor_planner/v1/measurement_pass.h
@@ -0,0 +1,54 @@
+// Copyright 2020-2022 The Electric Coin Company
+// Copyright 2022 The Halo2 developers
+// Use of this source code is governed by a MIT/Apache-2.0 style license that
+// can be found in the LICENSE-MIT.halo2 and the LICENCE-APACHE.halo2
+// file.
+
+#ifndef TACHYON_ZK_PLONK_CIRCUIT_FLOOR_PLANNER_V1_MEASUREMENT_PASS_H_
+#define TACHYON_ZK_PLONK_CIRCUIT_FLOOR_PLANNER_V1_MEASUREMENT_PASS_H_
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "tachyon/zk/plonk/circuit/region_shape.h"
+
+namespace tachyon::zk {
+
+// Measures the circuit.
+template <typename F>
+class MeasurementPass {
+ public:
+  using AssignRegionCallback = typename Layouter<F>::AssignRegionCallback;
+
+  struct Region {
+    Region(std::string_view name, RegionShape<F>&& shape)
+        : name(std::string(name)), shape(std::move(shape)) {}
+
+    std::string name;
+    RegionShape<F> shape;
+  };
+
+  MeasurementPass() = default;
+
+  const std::vector<Region>& regions() const { return regions_; }
+
+  void AssignRegion(std::string_view name, AssignRegionCallback assign) {
+    size_t region_index = regions_.size();
+
+    // Get shape of the region.
+    RegionShape<F> shape(region_index);
+    {
+      zk::Region<F> region(&shape);
+      assign.Run(region);
+    }
+    regions_.emplace_back(name, std::move(shape));
+  }
+
+ private:
+  std::vector<Region> regions_;
+};
+
+}  // namespace tachyon::zk
+
+#endif  // TACHYON_ZK_PLONK_CIRCUIT_FLOOR_PLANNER_V1_MEASUREMENT_PASS_H_
