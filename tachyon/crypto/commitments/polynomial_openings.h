1b2a777e56cf9165983f35d92d64b173da9cb738
Ryan Kim <ryan@lightscale.io>
2024-01-04 18:49:09 +0900

    chore(crypto): remove unused dependency

--- tachyon/crypto/commitments/polynomial_openings.h
+++ tachyon/crypto/commitments/polynomial_openings.h
@@ -5,7 +5,6 @@
 #include <vector>
 
 #include "absl/container/btree_set.h"
-#include "absl/container/flat_hash_map.h"
 #include "gtest/gtest_prod.h"
 
 #include "tachyon/base/containers/container_util.h"
