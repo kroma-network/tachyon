31b54a07e8782cc449a4bd1032249a25b3646b8a
dongchangYoo <yoodc8167@gmail.com>
2024-01-19 14:28:21 +0900

    feat(zk): implement `JsonConverter` for `LookupPair`

--- tachyon/zk/lookup/lookup_pair.h
+++ tachyon/zk/lookup/lookup_pair.h
@@ -7,10 +7,14 @@
 #ifndef TACHYON_ZK_LOOKUP_LOOKUP_PAIR_H_
 #define TACHYON_ZK_LOOKUP_LOOKUP_PAIR_H_
 
+#include <string>
 #include <utility>
 #include <vector>
 
-namespace tachyon::zk {
+#include "tachyon/base/json/json.h"
+
+namespace tachyon {
+namespace zk {
 
 template <typename T, typename U = T>
 class LookupPair {
@@ -38,6 +42,34 @@ class LookupPair {
 template <typename T, typename U = T>
 using LookupPairs = std::vector<LookupPair<T, U>>;
 
-}  // namespace tachyon::zk
+}  // namespace zk
+
+namespace base {
+
+template <typename T, typename U>
+class RapidJsonValueConverter<zk::LookupPair<T, U>> {
+ public:
+  template <typename Allocator>
+  static rapidjson::Value From(const zk::LookupPair<T, U>& value,
+                               Allocator& allocator) {
+    rapidjson::Value object(rapidjson::kObjectType);
+    AddJsonElement(object, "input", value.input(), allocator);
+    AddJsonElement(object, "table", value.table(), allocator);
+    return object;
+  }
+
+  static bool To(const rapidjson::Value& json_value, std::string_view key,
+                 zk::LookupPair<T, U>* value, std::string* error) {
+    T input;
+    U table;
+    if (!ParseJsonElement(json_value, "input", &input, error)) return false;
+    if (!ParseJsonElement(json_value, "table", &table, error)) return false;
+    *value = zk::LookupPair<T, U>(std::move(input), std::move(table));
+    return true;
+  }
+};
+
+}  // namespace base
+}  // namespace tachyon
 
 #endif  // TACHYON_ZK_LOOKUP_LOOKUP_PAIR_H_
