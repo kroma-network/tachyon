7044bb3e5c2ad098aecb82d9f575fdea5ef5ce49
Ryan Kim <ryan@lightscale.io>
2024-01-19 20:36:52 +0900

    feat(halo2): expose `XORShiftRng` from c++ to rust

--- vendors/halo2/src/circuit.rs
+++ vendors/halo2/src/circuit.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod test {
-    use crate::circuits::simple_circuit::SimpleCircuit;
     use crate::prover::create_proof as tachyon_create_proof;
+    use crate::{circuits::simple_circuit::SimpleCircuit, consts::SEED};
     use halo2_proofs::{
         circuit::Value,
         plonk::keygen_pk2,
@@ -46,10 +46,7 @@ mod test {
         let params = ParamsKZG::<Bn256>::unsafe_setup_with_s(k, s);
         let pk = keygen_pk2(&params, &circuit).expect("vk should not fail");
 
-        let rng = XorShiftRng::from_seed([
-            0x59, 0x62, 0xbe, 0x5d, 0x76, 0x3d, 0x31, 0x8d, 0x17, 0xdb, 0x37, 0x32, 0x54, 0x06,
-            0xbc, 0xe5,
-        ]);
+        let rng = XorShiftRng::from_seed(SEED);
 
         let proof = {
             let mut transcript = Blake2bWrite::<_, G1Affine, Challenge255<_>>::init(vec![]);
--- vendors/halo2/src/lib.rs
+++ vendors/halo2/src/lib.rs
@@ -1,6 +1,8 @@
 mod bn254;
 mod circuit;
 mod circuits;
+mod consts;
 mod msm;
 mod prover;
+mod xor_shift_rng;
 mod transcript;
