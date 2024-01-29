f9f6fc77ac8f3aca1c3280d7d38b43a467aebc40
Ryan Kim <ryan@lightscale.io>
2024-01-29 19:36:34 +0900

    feat(c/zk): add halo2 bn254 blake transcript apis

---
+++ tachyon/c/zk/plonk/halo2/bn254_transcript.cc
@@ -0,0 +1,102 @@
+#include "tachyon/c/zk/plonk/halo2/bn254_transcript.h"
+
+#include <string.h>
+
+#include <utility>
+#include <vector>
+
+#include "tachyon/base/logging.h"
+#include "tachyon/math/elliptic_curves/bn/bn254/g1.h"
+#include "tachyon/zk/plonk/halo2/blake2b_transcript.h"
+
+using namespace tachyon;
+
+tachyon_halo2_bn254_transcript_writer*
+tachyon_halo2_bn254_transcript_writer_create(uint8_t type) {
+  tachyon_halo2_bn254_transcript_writer* writer =
+      new tachyon_halo2_bn254_transcript_writer;
+  writer->type = type;
+  if (type == TACHYON_HALO2_BLAKE_TRANSCRIPT) {
+    base::Uint8VectorBuffer write_buf;
+    zk::halo2::Blake2bWriter<math::bn254::G1AffinePoint>* blake2b =
+        new zk::halo2::Blake2bWriter<math::bn254::G1AffinePoint>(
+            std::move(write_buf));
+    writer->extra = blake2b;
+  }
+  return writer;
+}
+
+tachyon_halo2_bn254_transcript_writer*
+tachyon_halo2_bn254_transcript_writer_create_from_state(uint8_t type,
+                                                        const uint8_t* state,
+                                                        size_t state_len) {
+  tachyon_halo2_bn254_transcript_writer* writer =
+      new tachyon_halo2_bn254_transcript_writer;
+  writer->type = type;
+  if (type == TACHYON_HALO2_BLAKE_TRANSCRIPT) {
+    base::Uint8VectorBuffer write_buf;
+    zk::halo2::Blake2bWriter<math::bn254::G1AffinePoint>* blake2b =
+        new zk::halo2::Blake2bWriter<math::bn254::G1AffinePoint>(
+            std::move(write_buf));
+    blake2b->SetState(absl::Span<const uint8_t>(state, state_len));
+    writer->extra = blake2b;
+  }
+  return writer;
+}
+
+void tachyon_halo2_bn254_transcript_writer_destroy(
+    tachyon_halo2_bn254_transcript_writer* writer) {
+  if (writer->type == TACHYON_HALO2_BLAKE_TRANSCRIPT) {
+    zk::halo2::Blake2bWriter<math::bn254::G1AffinePoint>* blake2b =
+        reinterpret_cast<zk::halo2::Blake2bWriter<math::bn254::G1AffinePoint>*>(
+            writer->extra);
+    delete blake2b;
+  }
+  delete writer;
+}
+
+void tachyon_halo2_bn254_transcript_writer_update(
+    tachyon_halo2_bn254_transcript_writer* writer, const uint8_t* data,
+    size_t data_len) {
+  if (writer->type == TACHYON_HALO2_BLAKE_TRANSCRIPT) {
+    zk::halo2::Blake2bWriter<math::bn254::G1AffinePoint>* blake2b =
+        reinterpret_cast<zk::halo2::Blake2bWriter<math::bn254::G1AffinePoint>*>(
+            writer->extra);
+    blake2b->Update(data, data_len);
+    return;
+  }
+  NOTREACHED();
+}
+
+void tachyon_halo2_bn254_transcript_writer_finalize(
+    tachyon_halo2_bn254_transcript_writer* writer, uint8_t* data,
+    size_t* data_len) {
+  if (writer->type == TACHYON_HALO2_BLAKE_TRANSCRIPT) {
+    *data_len = BLAKE2B512_DIGEST_LENGTH;
+    if (data == nullptr) return;
+    zk::halo2::Blake2bWriter<math::bn254::G1AffinePoint>* blake2b =
+        reinterpret_cast<zk::halo2::Blake2bWriter<math::bn254::G1AffinePoint>*>(
+            writer->extra);
+    uint8_t data_tmp[BLAKE2B512_DIGEST_LENGTH];
+    blake2b->Finalize(data_tmp);
+    memcpy(data, data_tmp, BLAKE2B512_DIGEST_LENGTH);
+    return;
+  }
+  NOTREACHED();
+}
+
+void tachyon_halo2_bn254_transcript_writer_get_state(
+    const tachyon_halo2_bn254_transcript_writer* writer, uint8_t* state,
+    size_t* state_len) {
+  if (writer->type == TACHYON_HALO2_BLAKE_TRANSCRIPT) {
+    *state_len = sizeof(blake2b_state_st);
+    if (state == nullptr) return;
+    zk::halo2::Blake2bWriter<math::bn254::G1AffinePoint>* blake2b =
+        reinterpret_cast<zk::halo2::Blake2bWriter<math::bn254::G1AffinePoint>*>(
+            writer->extra);
+    std::vector<uint8_t> state_tmp = blake2b->GetState();
+    memcpy(state, state_tmp.data(), sizeof(blake2b_state_st));
+    return;
+  }
+  NOTREACHED();
+}
---
+++ tachyon/c/zk/plonk/halo2/bn254_transcript.h
@@ -0,0 +1,51 @@
+#ifndef TACHYON_C_ZK_PLONK_HALO2_BN254_TRANSCRIPT_H_
+#define TACHYON_C_ZK_PLONK_HALO2_BN254_TRANSCRIPT_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "tachyon/c/export.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define TACHYON_HALO2_BLAKE_TRANSCRIPT 0
+
+struct tachyon_halo2_bn254_transcript_writer {
+  uint8_t type;
+  void* extra;
+};
+
+TACHYON_C_EXPORT tachyon_halo2_bn254_transcript_writer*
+tachyon_halo2_bn254_transcript_writer_create(uint8_t type);
+
+TACHYON_C_EXPORT tachyon_halo2_bn254_transcript_writer*
+tachyon_halo2_bn254_transcript_writer_create_from_state(uint8_t type,
+                                                        const uint8_t* state,
+                                                        size_t state_len);
+
+TACHYON_C_EXPORT void tachyon_halo2_bn254_transcript_writer_destroy(
+    tachyon_halo2_bn254_transcript_writer* writer);
+
+TACHYON_C_EXPORT void tachyon_halo2_bn254_transcript_writer_update(
+    tachyon_halo2_bn254_transcript_writer* writer, const uint8_t* data,
+    size_t data_len);
+
+// If |data| is NULL, then it populates |data_len| with length to be used.
+// If |data| is not NULL, then it populates |data| with the hash.
+TACHYON_C_EXPORT void tachyon_halo2_bn254_transcript_writer_finalize(
+    tachyon_halo2_bn254_transcript_writer* writer, uint8_t* data,
+    size_t* data_len);
+
+// If |state| is NULL, then it populates |state_len| with length to be used.
+// If |state| is not NULL, then it populates |state| with its internal state.
+TACHYON_C_EXPORT void tachyon_halo2_bn254_transcript_writer_get_state(
+    const tachyon_halo2_bn254_transcript_writer* writer, uint8_t* state,
+    size_t* state_len);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif  // TACHYON_C_ZK_PLONK_HALO2_BN254_TRANSCRIPT_H_
