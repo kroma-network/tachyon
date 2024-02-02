c3a61e41c9b1d95cd8022b9a774b0c62983867e3
Ryan Kim <ryan@lightscale.io>
2024-02-02 17:01:00 +0900

    refac(zk): move `kXORShiftSeed` to `zk/plon/halo2/constants.h`

--- tachyon/zk/plonk/halo2/constants.h
+++ tachyon/zk/plonk/halo2/constants.h
@@ -30,6 +30,10 @@ constexpr uint8_t kShaPrefixScalar[1] = {2};
 
 constexpr char kVerifyingKeyStr[] = "Halo2-Verify-Key";
 
+constexpr uint8_t kXORShiftSeed[] = {0x59, 0x62, 0xbe, 0x5d, 0x76, 0x3d,
+                                     0x31, 0x8d, 0x17, 0xdb, 0x37, 0x32,
+                                     0x54, 0x06, 0xbc, 0xe5};
+
 }  // namespace tachyon::zk::plonk::halo2
 
 #endif  // TACHYON_ZK_PLONK_HALO2_CONSTANTS_H_
