1ebd6e4fb70c80c04cbf978c694e5db7e029fa63
Ryan Kim <ryan@lightscale.io>
2024-02-02 18:28:30 +0900

    refac(c/zk): extract `ProvingKeyImpl` to be shared in other places

--- tachyon/c/zk/plonk/keys/bn254_plonk_proving_key.cc
+++ tachyon/c/zk/plonk/keys/bn254_plonk_proving_key.cc
@@ -1,32 +1,13 @@
 #include "tachyon/c/zk/plonk/keys/bn254_plonk_proving_key.h"
 
-#include "tachyon/c/math/polynomials/constants.h"
-#include "tachyon/c/zk/plonk/keys/proving_key_impl_base.h"
-#include "tachyon/math/elliptic_curves/bn/bn254/g1.h"
-#include "tachyon/math/polynomials/univariate/univariate_evaluations.h"
-#include "tachyon/math/polynomials/univariate/univariate_polynomial.h"
+#include "tachyon/c/zk/plonk/keys/bn254_plonk_proving_key_impl.h"
 #include "tachyon/zk/plonk/keys/proving_key.h"
 
 using namespace tachyon;
 
 namespace {
 
-// NOTE(chokobole): It assumes that proving key has univariate dense polynomial
-// and evaluations.
-using Poly =
-    math::UnivariateDensePolynomial<math::bn254::Fr, c::math::kMaxDegree>;
-using Evals = math::UnivariateEvaluations<math::bn254::Fr, c::math::kMaxDegree>;
-
-class Bn254ProvingKeyImpl
-    : public c::zk::plonk::ProvingKeyImplBase<Poly, Evals,
-                                              math::bn254::G1AffinePoint> {
- public:
-  explicit Bn254ProvingKeyImpl(absl::Span<const uint8_t> state)
-      : c::zk::plonk::ProvingKeyImplBase<Poly, Evals,
-                                         math::bn254::G1AffinePoint>(state) {}
-};
-
-using PKeyImpl = Bn254ProvingKeyImpl;
+using PKeyImpl = c::zk::plonk::bn254::ProvingKeyImpl;
 
 }  // namespace
 
