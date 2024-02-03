e9a161bf749fc1fb9ee05195e395528d69fdb717
Wonyong Kim <chokobole33@gmail.com>
2024-02-04 00:38:35 +0900

    feat(zk): implement groth16 `VerifyingKey` and `PreparedVerifyingKey`
    See
    [VerifyingKey](https://github.com/arkworks-rs/groth16/blob/8e5c347bd8776645e046ca7ec1e4b9ff4b97c054/src/data_structures.rs#L30-L56),
    [PreparedVerifyingKey](https://github.com/arkworks-rs/groth16/blob/8e5c347bd8776645e046ca7ec1e4b9ff4b97c054/src/data_structures.rs#L85-L120)
    and some parts of
    [Groth16::generate_parameters_with_qap](https://github.com/arkworks-rs/groth16/blob/8e5c347bd8776645e046ca7ec1e4b9ff4b97c054/src/generator.rs#L46-L209).

---
+++ tachyon/zk/r1cs/groth16/prepared_verifying_key.h
@@ -0,0 +1,72 @@
+// Copyright 2022 arkworks contributors
+// Use of this source code is governed by a MIT/Apache-2.0 style license that
+// can be found in the LICENSE-MIT.arkworks and the LICENCE-APACHE.arkworks
+// file.
+
+#ifndef TACHYON_ZK_R1CS_GROTH16_PREPARED_VERIFYING_KEY_H_
+#define TACHYON_ZK_R1CS_GROTH16_PREPARED_VERIFYING_KEY_H_
+
+#include <utility>
+
+#include "tachyon/zk/r1cs/groth16/verifying_key.h"
+
+namespace tachyon::zk::r1cs::groth16 {
+
+template <typename Curve>
+class PreparedVerifyingKey {
+ public:
+  using G2Prepared = typename Curve::G2Prepared;
+  using Fp12 = typename Curve::Fp12;
+
+  PreparedVerifyingKey() = default;
+  PreparedVerifyingKey(const VerifyingKey<Curve>& verifying_key,
+                       const Fp12& alpha_g1_beta_g2,
+                       const G2Prepared& delta_neg_g2,
+                       const G2Prepared& gamma_neg_g2)
+      : verifying_key_(verifying_key),
+        alpha_g1_beta_g2_(alpha_g1_beta_g2),
+        delta_neg_g2_(delta_neg_g2),
+        gamma_neg_g2_(gamma_neg_g2) {}
+  PreparedVerifyingKey(VerifyingKey<Curve>&& verifying_key,
+                       Fp12&& alpha_g1_beta_g2, G2Prepared&& delta_neg_g2,
+                       G2Prepared&& gamma_neg_g2)
+      : verifying_key_(std::move(verifying_key)),
+        alpha_g1_beta_g2_(std::move(alpha_g1_beta_g2)),
+        delta_neg_g2_(std::move(delta_neg_g2)),
+        gamma_neg_g2_(std::move(gamma_neg_g2)) {}
+
+  const VerifyingKey<Curve>& verifying_key() const { return verifying_key_; }
+  const Fp12& alpha_g1_beta_g2() const { return alpha_g1_beta_g2_; }
+  const G2Prepared& delta_neg_g2() const { return delta_neg_g2_; }
+  const G2Prepared& gamma_neg_g2() const { return gamma_neg_g2_; }
+
+ private:
+  VerifyingKey<Curve> verifying_key_;
+  // e([α]₁, [β]₂)
+  Fp12 alpha_g1_beta_g2_;
+  // [-δ]₂
+  G2Prepared delta_neg_g2_;
+  // [-γ]₂
+  G2Prepared gamma_neg_g2_;
+};
+
+template <typename Curve>
+PreparedVerifyingKey<Curve> VerifyingKey<Curve>::ToPreparedVerifyingKey() && {
+  using G1AffinePoint = typename Curve::G1Curve::AffinePoint;
+  using G2AffinePoint = typename Curve::G2Curve::AffinePoint;
+  using G2Prepared = typename Curve::G2Prepared;
+  using Fp12 = typename Curve::Fp12;
+
+  G1AffinePoint left[] = {alpha_g1_};
+  G2AffinePoint right[] = {beta_g2_};
+  Fp12 alpha_g1_beta_g2 = math::Pairing<Curve>(left, right);
+  G2Prepared delta_neg_g2 = G2Prepared::From(-delta_g2_);
+  G2Prepared gamma_neg_g2 = G2Prepared::From(-gamma_g2_);
+
+  return {std::move(*this), std::move(alpha_g1_beta_g2),
+          std::move(delta_neg_g2), std::move(gamma_neg_g2)};
+}
+
+}  // namespace tachyon::zk::r1cs::groth16
+
+#endif  // TACHYON_ZK_R1CS_GROTH16_PREPARED_VERIFYING_KEY_H_
