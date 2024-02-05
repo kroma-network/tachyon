d6158243ae8e23f43c1c2a3cc41523226b62b73b
Ryan Kim <ryan@lightscale.io>
2024-02-05 20:15:47 +0900

    feat(c/zk): add `tachyon_halo2_bn254_shplonk_prover_create_from_params`

--- tachyon/c/zk/plonk/halo2/bn254_shplonk_prover.cc
+++ tachyon/c/zk/plonk/halo2/bn254_shplonk_prover.cc
@@ -62,6 +62,41 @@ tachyon_halo2_bn254_shplonk_prover_create_from_unsafe_setup(
   return reinterpret_cast<tachyon_halo2_bn254_shplonk_prover*>(prover);
 }
 
+tachyon_halo2_bn254_shplonk_prover*
+tachyon_halo2_bn254_shplonk_prover_create_from_params(uint8_t transcript_type,
+                                                      uint32_t k,
+                                                      const uint8_t* params,
+                                                      size_t params_len) {
+  math::bn254::BN254Curve::Init();
+
+  ProverImpl* prover = new ProverImpl(
+      [transcript_type, k, params, params_len]() {
+        PCS pcs;
+        base::ReadOnlyBuffer read_buf(params, params_len);
+        CHECK(read_buf.Read(&pcs));
+
+        base::Uint8VectorBuffer write_buf;
+        std::unique_ptr<crypto::TranscriptWriter<math::bn254::G1AffinePoint>>
+            writer;
+        if (transcript_type == TACHYON_HALO2_BLAKE_TRANSCRIPT) {
+          writer = std::make_unique<
+              zk::plonk::halo2::Blake2bWriter<math::bn254::G1AffinePoint>>(
+              std::move(write_buf));
+        } else {
+          NOTREACHED();
+        }
+        zk::plonk::halo2::Prover<PCS> prover =
+            zk::plonk::halo2::Prover<PCS>::CreateFromRNG(
+                std::move(pcs), std::move(writer),
+                /*rng=*/nullptr,
+                /*blinding_factors=*/0);
+        prover.set_domain(PCS::Domain::Create(size_t{1} << k));
+        return prover;
+      },
+      transcript_type);
+  return reinterpret_cast<tachyon_halo2_bn254_shplonk_prover*>(prover);
+}
+
 void tachyon_halo2_bn254_shplonk_prover_destroy(
     tachyon_halo2_bn254_shplonk_prover* prover) {
   delete reinterpret_cast<ProverImpl*>(prover);
