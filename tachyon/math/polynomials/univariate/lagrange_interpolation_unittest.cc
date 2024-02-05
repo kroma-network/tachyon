11ca08d163cbac28b545137aa64c90347aaa63cc
Wonyong Kim <chokobole33@gmail.com>
2024-02-06 01:08:46 +0900

    fix(math): correct parallel lagrange interpolation
    When openmp is activated, two bugs are present in the Lagrange interpolation:
    
    1. When computing `denoms`, the index for comparison was incorrect. It should
        have obtained the index using `chunk_offset * chunk_size`.
    2. When computing `final_coeffs`, it attempted to access `chunk` with
        the index of `numerators`, not the index of `chunk`.
    
    In addition to these bugs, the comments provided incorrect descriptions.
    
    Upon examining the computation of numerator parts, it becomes apparent that
    this is a slightly modified version of
    `UnivariateDenseCoefficients::FromRoots()`. Therefore, we have chosen to use
    this code for both better maintenance and bug fixes.

--- tachyon/math/polynomials/univariate/lagrange_interpolation_unittest.cc
+++ tachyon/math/polynomials/univariate/lagrange_interpolation_unittest.cc
@@ -2,41 +2,31 @@
 
 #include "gtest/gtest.h"
 
-#include "tachyon/math/finite_fields/test/gf7.h"
+#include "tachyon/math/elliptic_curves/bn/bn254/fr.h"
 
 namespace tachyon::math {
 
 TEST(LagrangeInterpolationTest, LagrangeInterpolate) {
-  using F = math::GF7;
+  using F = bn254::Fr;
   F::Init();
 
-  // Lagrange polynomial for (1, 2), (2, 0), (3, 0)
-  std::vector<F> points_3({F(1), F(2), F(3)});
-  std::vector<F> evals_3({F(2), F(0), F(0)});
+#if defined(TACHYON_HAS_OPENMP)
+  constexpr size_t kDegree = 128;
+#else
+  constexpr size_t kDegree = 4;
+#endif
 
-  // Expected: (x - 2)(x - 3) = x² - 5x + 6 = x² + 2x + 6
-  UnivariateDensePolynomial<F, 2> expected_2d_poly =
-      UnivariateDensePolynomial<F, 2>(
-          UnivariateDenseCoefficients<F, 2>({F(6), F(2), F(1)}));
+  std::vector<F> points =
+      base::CreateVector(kDegree + 1, [](size_t i) { return F(i); });
+  std::vector<F> evals =
+      base::CreateVector(kDegree + 1, []() { return F::Random(); });
 
-  UnivariateDensePolynomial<F, 2> actual_2d_poly;
-  EXPECT_TRUE(LagrangeInterpolate(points_3, evals_3, &actual_2d_poly));
+  UnivariateDensePolynomial<F, kDegree> poly;
+  EXPECT_TRUE(LagrangeInterpolate(points, evals, &poly));
 
-  EXPECT_EQ(expected_2d_poly, actual_2d_poly);
-
-  // Lagrange polynomial for (0,4), (1,3), (2,5), (6,2)
-  std::vector<F> points_4({F(0), F(1), F(2), F(6)});
-  std::vector<F> evals_4({F(4), F(3), F(5), F(2)});
-
-  // Expected: x³ + 2x² + 3x + 4
-  UnivariateDensePolynomial<F, 3> expected_3d_poly =
-      UnivariateDensePolynomial<F, 3>(
-          UnivariateDenseCoefficients<F, 3>({F(4), F(3), F(2), F(1)}));
-
-  UnivariateDensePolynomial<F, 3> actual_3d_poly;
-  EXPECT_TRUE(LagrangeInterpolate(points_4, evals_4, &actual_3d_poly));
-
-  EXPECT_EQ(expected_3d_poly, actual_3d_poly);
+  OPENMP_PARALLEL_FOR(size_t i = 0; i < points.size(); ++i) {
+    EXPECT_EQ(poly.Evaluate(points[i]), evals[i]);
+  }
 }
 
 }  // namespace tachyon::math
