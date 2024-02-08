7474c5e4f1bc79f72ef412e99cdcac79eccd7b37
Ryan Kim <ryan@lightscale.io>
2024-02-08 09:21:17 +0900

    test(zk): use the same pattern to create vector

--- tachyon/zk/plonk/vanishing/vanishing_utils_unittest.cc
+++ tachyon/zk/plonk/vanishing/vanishing_utils_unittest.cc
@@ -65,9 +65,8 @@ TEST_F(VanishingUtilsTest, CoeffToExtendedPart) {
 }
 
 TEST_F(VanishingUtilsTest, BuildExtendedColumnWithColumns) {
-  base::Range<size_t> range = base::Range<size_t>::Until(4);
-  std::vector<std::vector<F>> columns =
-      base::Map(range, [](size_t i) { return base::CreateVector(N, F(i)); });
+  std::vector<std::vector<F>> columns = base::CreateVector(
+      4, [](size_t i) { return base::CreateVector(N, F(i)); });
 
   std::vector<F> extended = BuildExtendedColumnWithColumns(columns);
   EXPECT_EQ(extended.size(), 4 * N);
