d79068ad9f5a0fe11c9904311011444c04d871ca
ashjeong <ashjeong@umich.edu>
2024-02-12 13:59:13 +0900

    feat: add multilinear sumcheck data structures
    Referenced [arkworks' sumcheck code](https://github.com/arkworks-rs/sumcheck/blob/master/src/ml_sumcheck/data_structures.rs)

---
+++ tachyon/math/polynomials/multivariate/linear_combination.h
@@ -0,0 +1,125 @@
+#ifndef TACHYON_MATH_POLYNOMIALS_MULTIVARIATE_LINEAR_COMBINATION_H_
+#define TACHYON_MATH_POLYNOMIALS_MULTIVARIATE_LINEAR_COMBINATION_H_
+
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <numeric>
+#include <utility>
+#include <vector>
+
+#include "absl/container/node_hash_map.h"
+
+#include "tachyon/base/containers/container_util.h"
+#include "tachyon/base/parallelize.h"
+#include "tachyon/base/random.h"
+#include "tachyon/math/polynomials/multivariate/linear_combination_term.h"
+
+namespace tachyon::math {
+
+template <typename MLE>
+class LinearCombination {
+ public:
+  using F = typename MLE::Field;
+  using Point = typename MLE::Point;
+
+  LinearCombination() = default;
+  explicit LinearCombination(size_t num_variables)
+      : num_variables_(num_variables) {}
+  LinearCombination(LinearCombination& other) = default;
+  LinearCombination& operator=(LinearCombination& other) = default;
+
+  size_t max_evaluations() const { return max_evaluations_; }
+  size_t num_variables() const { return num_variables_; }
+  const std::vector<LinearCombinationTerm<F>>& terms() const { return terms_; }
+  const std::vector<std::shared_ptr<MLE>>& flattened_ml_evaluations() const {
+    return flattened_ml_evaluations_;
+  }
+
+  static LinearCombination Random(size_t num_variables,
+                                  base::Range<size_t> max_evaluations_range,
+                                  size_t num_terms) {
+    LinearCombination linear_combination(num_variables);
+
+    size_t max_evaluations = base::Uniform(max_evaluations_range);
+    for (size_t i = 0; i < num_terms; ++i) {
+      F coefficient = F::Random();
+      std::vector<std::shared_ptr<MLE>> evaluations =
+          base::CreateVector(max_evaluations, [num_variables]() {
+            return std::make_shared<MLE>(MLE::Random(num_variables));
+          });
+      linear_combination.AddTerm(coefficient, evaluations);
+    }
+    return linear_combination;
+  }
+
+  // Adds a |LinearCombinationTerm| to member |terms_|
+  void AddTerm(const F& coefficient,
+               const std::vector<std::shared_ptr<MLE>>& evaluations) {
+    if (evaluations.empty()) return;
+    size_t num_evaluations = evaluations.size();
+    max_evaluations_ = std::max(max_evaluations_, num_evaluations);
+    std::vector<size_t> indexes =
+        base::Map(evaluations, [this](const std::shared_ptr<MLE>& evaluation) {
+          CHECK_EQ(evaluation->Degree(), num_variables_);
+          const MLE* evaluation_ptr = evaluation.get();
+          auto it = lookup_table_.try_emplace(evaluation_ptr,
+                                              flattened_ml_evaluations_.size());
+          // if inserted
+          if (it.second) {
+            flattened_ml_evaluations_.push_back(evaluation);
+          }
+          return (*it.first).second;
+        });
+    terms_.push_back({coefficient, std::move(indexes)});
+  }
+
+  // Evaluates all terms together on an evaluation point for each variable:
+  // ∑ᵢ₌₀..ₙ(coefficientᵢ⋅∏ⱼ₌₀..ₘevaluationᵢⱼ)
+  // where n = total terms, m = number of evaluations per term
+  F SumOfProducts(const Point& point) const {
+    CHECK_EQ(point.size(), num_variables_);
+#if defined(TACHYON_HAS_OPENMP)
+    std::vector<F> results = base::ParallelizeMap(
+        terms_,
+        [this, &point](absl::Span<const LinearCombinationTerm<F>> chunk) {
+          return EvaluateSerial(point, flattened_ml_evaluations_, chunk);
+        });
+    return std::accumulate(results.begin(), results.end(), F::Zero(),
+                           std::plus<>());
+#else
+    return EvaluateSerial(point, flattened_ml_evaluations_, terms_);
+#endif
+  }
+
+ private:
+  // max number of evaluations for each term
+  size_t max_evaluations_ = 0;
+  // number of variables of the polynomial
+  size_t num_variables_ = 0;
+  // list of coefficients and their corresponding list of indexes in reference
+  // to evaluations in |flattened_ml_evaluations_|
+  std::vector<LinearCombinationTerm<F>> terms_;
+  // holds shared pointers of unique multilinear evaluations
+  std::vector<std::shared_ptr<MLE>> flattened_ml_evaluations_;
+  // holds all unique dense multilinear evaluations currently used & their
+  // corresponding index in |flattened_ml_evaluations_|
+  // not owned
+  absl::node_hash_map<const MLE*, size_t> lookup_table_;
+
+  static F EvaluateSerial(
+      const Point& point,
+      const std::vector<std::shared_ptr<MLE>>& flattened_ml_evaluations,
+      absl::Span<const LinearCombinationTerm<F>> terms) {
+    return std::accumulate(terms.begin(), terms.end(), F::Zero(),
+                           [&point, &flattened_ml_evaluations](
+                               F& acc, const LinearCombinationTerm<F>& term) {
+                             return acc += term.Evaluate(
+                                        point, flattened_ml_evaluations);
+                           });
+  }
+};
+
+}  // namespace tachyon::math
+
+#endif  // TACHYON_MATH_POLYNOMIALS_MULTIVARIATE_LINEAR_COMBINATION_H_
