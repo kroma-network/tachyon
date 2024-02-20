03f3aff514d9df599e2eed39abc98392837cf8f5
Ryan Kim <ryan@lightscale.io>
2024-02-20 10:26:25 +0900

    test(halo2): add test cases for the prover with poseidon and sha256 transcript

--- vendors/halo2/src/circuits/simple_circuit.rs
+++ vendors/halo2/src/circuits/simple_circuit.rs
@@ -305,12 +305,15 @@ impl<F: FieldExt> Circuit<F> for SimpleCircuit<F> {
 #[cfg(test)]
 mod test {
     use crate::bn254::{
-        Blake2bWrite as TachyonBlake2bWrite, ProvingKey as TachyonProvingKey, SHPlonkProver,
+        Blake2bWrite as TachyonBlake2bWrite, PoseidonWrite as TachyonPoseidonWrite,
+        ProvingKey as TachyonProvingKey, SHPlonkProver, Sha256Write as TachyonSha256Write,
     };
     use crate::circuits::simple_circuit::SimpleCircuit;
     use crate::consts::{TranscriptType, SEED};
     use crate::prover::create_proof as tachyon_create_proof;
+    use crate::sha::ShaWrite;
     use crate::xor_shift_rng::XORShiftRng;
+    use halo2_proofs::transcript::PoseidonWrite;
     use halo2_proofs::{
         circuit::Value,
         plonk::keygen_pk2,
@@ -353,10 +356,13 @@ mod test {
         let s = Fr::from(2);
         let params = ParamsKZG::<Bn256>::unsafe_setup_with_s(k, s.clone());
         let pk = keygen_pk2(&params, &circuit).expect("vk should not fail");
+        let mut pk_bytes: Vec<u8> = vec![];
+        pk.write(&mut pk_bytes, halo2_proofs::SerdeFormat::RawBytesUnchecked)
+            .unwrap();
 
         let rng = XORShiftRng::from_seed(SEED);
 
-        let halo2_proof = {
+        let halo2_blake2b_proof = {
             let mut transcript = Blake2bWrite::<_, G1Affine, Challenge255<_>>::init(vec![]);
 
             halo2_proofs::plonk::create_proof::<
@@ -379,25 +385,22 @@ mod test {
             transcript.finalize()
         };
 
-        let tachyon_proof = {
+        let tachyon_blake2b_proof = {
             let mut prover = SHPlonkProver::<KZGCommitmentScheme<Bn256>>::new(
                 TranscriptType::Blake2b as u8,
                 k,
                 &s,
             );
 
-            let mut pk_bytes: Vec<u8> = vec![];
-            pk.write(&mut pk_bytes, halo2_proofs::SerdeFormat::RawBytesUnchecked)
-                .unwrap();
             let mut tachyon_pk = TachyonProvingKey::from(pk_bytes.as_slice());
             let mut transcript = TachyonBlake2bWrite::init(vec![]);
 
             tachyon_create_proof::<_, _, _, _>(
                 &mut prover,
                 &mut tachyon_pk,
-                &[circuit],
+                &[circuit.clone()],
                 public_inputs3.as_slice(),
-                rng,
+                rng.clone(),
                 &mut transcript,
             )
             .expect("proof generation should not fail");
@@ -407,7 +410,108 @@ mod test {
             proof.extend_from_slice(&proof_last);
             proof
         };
-        assert_eq!(halo2_proof, tachyon_proof);
+        assert_eq!(halo2_blake2b_proof, tachyon_blake2b_proof);
+
+        let halo2_poseidon_proof = {
+            let mut transcript = PoseidonWrite::<_, G1Affine, Challenge255<_>>::init(vec![]);
+
+            halo2_proofs::plonk::create_proof::<
+                KZGCommitmentScheme<Bn256>,
+                ProverSHPLONK<_>,
+                _,
+                _,
+                _,
+                _,
+            >(
+                &params,
+                &pk,
+                &[circuit.clone()],
+                public_inputs3.as_slice(),
+                rng.clone(),
+                &mut transcript,
+            )
+            .expect("proof generation should not fail");
+
+            transcript.finalize()
+        };
+
+        let tachyon_poseidon_proof = {
+            let mut prover = SHPlonkProver::<KZGCommitmentScheme<Bn256>>::new(
+                TranscriptType::Poseidon as u8,
+                k,
+                &s,
+            );
+
+            let mut tachyon_pk = TachyonProvingKey::from(pk_bytes.as_slice());
+            let mut transcript = TachyonPoseidonWrite::init(vec![]);
+
+            tachyon_create_proof::<_, _, _, _>(
+                &mut prover,
+                &mut tachyon_pk,
+                &[circuit.clone()],
+                public_inputs3.as_slice(),
+                rng.clone(),
+                &mut transcript,
+            )
+            .expect("proof generation should not fail");
+
+            let mut proof = transcript.finalize();
+            let proof_last = prover.get_proof();
+            proof.extend_from_slice(&proof_last);
+            proof
+        };
+        assert_eq!(halo2_poseidon_proof, tachyon_poseidon_proof);
+
+        let halo2_sha256_proof = {
+            let mut transcript =
+                ShaWrite::<_, G1Affine, Challenge255<_>, sha2::Sha256>::init(vec![]);
+
+            halo2_proofs::plonk::create_proof::<
+                KZGCommitmentScheme<Bn256>,
+                ProverSHPLONK<_>,
+                _,
+                _,
+                _,
+                _,
+            >(
+                &params,
+                &pk,
+                &[circuit.clone()],
+                public_inputs3.as_slice(),
+                rng.clone(),
+                &mut transcript,
+            )
+            .expect("proof generation should not fail");
+
+            transcript.finalize()
+        };
+
+        let tachyon_sha256_proof = {
+            let mut prover = SHPlonkProver::<KZGCommitmentScheme<Bn256>>::new(
+                TranscriptType::Sha256 as u8,
+                k,
+                &s,
+            );
+
+            let mut tachyon_pk = TachyonProvingKey::from(pk_bytes.as_slice());
+            let mut transcript = TachyonSha256Write::init(vec![]);
+
+            tachyon_create_proof::<_, _, _, _>(
+                &mut prover,
+                &mut tachyon_pk,
+                &[circuit.clone()],
+                public_inputs3.as_slice(),
+                rng.clone(),
+                &mut transcript,
+            )
+            .expect("proof generation should not fail");
+
+            let mut proof = transcript.finalize();
+            let proof_last = prover.get_proof();
+            proof.extend_from_slice(&proof_last);
+            proof
+        };
+        assert_eq!(halo2_sha256_proof, tachyon_sha256_proof);
         // ANCHOR_END: test-circuit
     }
 }
