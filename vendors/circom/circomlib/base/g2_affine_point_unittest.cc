a6a28bd5cfb8f1f7a35ebd3f9d779803787d9bda
Wonyong Kim <chokobole33@gmail.com>
2024-03-11 08:13:32 +0900

    feat(circom): add `G2AffinePoint`

---
+++ vendors/circom/circomlib/base/g2_affine_point_unittest.cc
@@ -0,0 +1,21 @@
+#include "circomlib/base/g2_affine_point.h"
+
+#include "gtest/gtest.h"
+
+#include "tachyon/math/elliptic_curves/bn/bn254/g2.h"
+#include "tachyon/math/finite_fields/test/finite_field_test.h"
+
+namespace tachyon::circom {
+
+class G2AffinePointTest : public testing::Test {
+ public:
+  static void SetUpTestSuite() { math::bn254::G2Curve::Init(); }
+};
+
+TEST_F(G2AffinePointTest, Conversions) {
+  math::bn254::G2AffinePoint expected = math::bn254::G2AffinePoint::Random();
+  G2AffinePoint affine_point = G2AffinePoint::FromNative(expected);
+  EXPECT_EQ(affine_point.ToNative<math::bn254::G2Curve>(), expected);
+}
+
+}  // namespace tachyon::circom
