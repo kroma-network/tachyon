e2ce08a3cce3df4ffd4d17f0bc308d5a9f3536e4
Ryan Kim <chokobole33@gmail.com>
2024-04-13 02:35:53 +0900

    refac(math): remove unused headers

--- tachyon/math/elliptic_curves/affine_point.h
+++ tachyon/math/elliptic_curves/affine_point.h
@@ -3,10 +3,8 @@
 
 #include <utility>
 
-#include "tachyon/base/buffer/copyable.h"
 #include "tachyon/math/elliptic_curves/jacobian_point.h"
 #include "tachyon/math/elliptic_curves/point_conversions_forward.h"
-#include "tachyon/math/geometry/point2.h"
 
 namespace tachyon::math {
 
