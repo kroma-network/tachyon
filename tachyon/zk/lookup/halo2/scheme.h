3ab7ec786aeb2f2fe4d35527fe132ed7dc8dab2e
Insun35 <inseonyu7@gmail.com>
2024-04-25 15:14:13 +0900

    feat(zk): add Halo2 Lookup Scheme
    Introduce LookupScheme interface for abstraction.
    LookupScheme holds types of each LookupProver, LookupVerifier,
    LookupVerifierData and LookupEvaluator.
    
    These members can be only used in Halo2 LookupScheme.

---
+++ tachyon/zk/lookup/halo2/scheme.h
@@ -0,0 +1,29 @@
+#ifndef TACHYON_ZK_LOOKUP_HALO2_SCHEME_H_
+#define TACHYON_ZK_LOOKUP_HALO2_SCHEME_H_
+
+#include "tachyon/zk/lookup/halo2/evaluator.h"
+#include "tachyon/zk/lookup/halo2/prover.h"
+#include "tachyon/zk/lookup/halo2/verifier.h"
+#include "tachyon/zk/lookup/halo2/verifier_data.h"
+#include "tachyon/zk/lookup/type.h"
+
+namespace tachyon::zk::lookup::halo2 {
+
+template <typename _Poly, typename _Evals, typename _Commitment>
+struct Scheme {
+  using Poly = _Poly;
+  using Evals = _Evals;
+  using Commitment = _Commitment;
+  using Field = typename Poly::Field;
+
+  using Prover = lookup::halo2::Prover<Poly, Evals>;
+  using Verifier = lookup::halo2::Verifier<Field, Commitment>;
+  using VerifierData = lookup::halo2::VerifierData<Field, Commitment>;
+  using Evaluator = lookup::halo2::Evaluator<Field, Evals>;
+
+  static constexpr Type type = Type::kHalo2;
+};
+
+}  // namespace tachyon::zk::lookup::halo2
+
+#endif  // TACHYON_ZK_LOOKUP_HALO2_SCHEME_H_
