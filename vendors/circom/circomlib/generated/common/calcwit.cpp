450596bcb9674de1b41c48b2cc9bb46d8c8699d4
Ryan Kim <chokobole33@gmail.com>
2024-04-29 14:56:26 +0900

    feat(circom): enable loading witness from json

--- vendors/circom/circomlib/generated/common/calcwit.cpp
+++ vendors/circom/circomlib/generated/common/calcwit.cpp
@@ -12,6 +12,9 @@
 #include <vector>
 #include <chrono>
 #include "calcwit.hpp"
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
 
 extern void run(Circom_CalcWit* ctx);
 
@@ -211,6 +214,109 @@ Circom_Circuit* loadCircuit(std::string const &datFileName) {
   munmap(bdata, sb.st_size);
 
   return circuit;
+}
+
+bool check_valid_number(std::string & s, uint base){
+  bool is_valid = true;
+  if (base == 16){
+    for (uint i = 0; i < s.size(); i++){
+      is_valid &= (
+        ('0' <= s[i] && s[i] <= '9') ||
+        ('a' <= s[i] && s[i] <= 'f') ||
+        ('A' <= s[i] && s[i] <= 'F')
+      );
+    }
+  } else{
+    for (uint i = 0; i < s.size(); i++){
+      is_valid &= ('0' <= s[i] && s[i] < char(int('0') + base));
+    }
+  }
+  return is_valid;
+}
+
+void json2FrElements (json val, std::vector<FrElement> & vval){
+  if (!val.is_array()) {
+    FrElement v;
+    std::string s_aux, s;
+    uint base;
+    if (val.is_string()) {
+      s_aux = val.get<std::string>();
+      std::string possible_prefix = s_aux.substr(0, 2);
+      if (possible_prefix == "0b" || possible_prefix == "0B"){
+        s = s_aux.substr(2, s_aux.size() - 2);
+        base = 2;
+      } else if (possible_prefix == "0o" || possible_prefix == "0O"){
+        s = s_aux.substr(2, s_aux.size() - 2);
+        base = 8;
+      } else if (possible_prefix == "0x" || possible_prefix == "0X"){
+        s = s_aux.substr(2, s_aux.size() - 2);
+        base = 16;
+      } else{
+        s = s_aux;
+        base = 10;
+      }
+      if (!check_valid_number(s, base)){
+        std::ostringstream errStrStream;
+        errStrStream << "Invalid number in JSON input: " << s_aux << "\n";
+	      throw std::runtime_error(errStrStream.str() );
+      }
+    } else if (val.is_number()) {
+        double vd = val.get<double>();
+        std::stringstream stream;
+        stream << std::fixed << std::setprecision(0) << vd;
+        s = stream.str();
+        base = 10;
+    } else {
+        std::ostringstream errStrStream;
+        errStrStream << "Invalid JSON type\n";
+	      throw std::runtime_error(errStrStream.str() );
+    }
+    Fr_str2element (&v, s.c_str(), base);
+    vval.push_back(v);
+  } else {
+    for (uint i = 0; i < val.size(); i++) {
+      json2FrElements (val[i], vval);
+    }
+  }
+}
+
+void loadJson(Circom_CalcWit *ctx, std::string filename) {
+  std::ifstream inStream(filename);
+  json j;
+  inStream >> j;
+
+  u64 nItems = j.size();
+  // printf("Items : %llu\n",nItems);
+  if (nItems == 0){
+    ctx->tryRunCircuit();
+  }
+  for (json::iterator it = j.begin(); it != j.end(); ++it) {
+    // std::cout << it.key() << " => " << it.value() << '\n';
+    u64 h = fnv1a(it.key());
+    std::vector<FrElement> v;
+    json2FrElements(it.value(),v);
+    uint signalSize = ctx->getInputSignalSize(h);
+    if (v.size() < signalSize) {
+	std::ostringstream errStrStream;
+	errStrStream << "Error loading signal " << it.key() << ": Not enough values\n";
+	throw std::runtime_error(errStrStream.str() );
+    }
+    if (v.size() > signalSize) {
+	std::ostringstream errStrStream;
+	errStrStream << "Error loading signal " << it.key() << ": Too many values\n";
+	throw std::runtime_error(errStrStream.str() );
+    }
+    for (uint i = 0; i<v.size(); i++){
+      try {
+	// std::cout << it.key() << "," << i << " => " << Fr_element2str(&(v[i])) << '\n';
+	ctx->setInputSignal(h,i,v[i]);
+      } catch (std::runtime_error e) {
+	std::ostringstream errStrStream;
+	errStrStream << "Error setting signal: " << it.key() << "\n" << e.what();
+	throw std::runtime_error(errStrStream.str() );
+      }
+    }
+  }
 }
 
 void loadWitness(Circom_CalcWit *ctx, const absl::flat_hash_map<std::string, std::vector<FrElement>>& witness) {
