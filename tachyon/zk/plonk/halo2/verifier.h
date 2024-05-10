48f6af32e88ee27ae1e821b0e9b9040bed9405d4
ashjeong <ashjeong@umich.edu>
2024-05-10 15:56:30 +0900

    test(zk): remove circuit tests as prep

--- tachyon/zk/plonk/halo2/verifier.h
+++ tachyon/zk/plonk/halo2/verifier.h
@@ -48,13 +48,6 @@ class Verifier : public VerifierBase<PCS> {
   }
 
  private:
-  FRIEND_TEST(SimpleCircuitTest, Verify);
-  FRIEND_TEST(SimpleV1CircuitTest, Verify);
-  FRIEND_TEST(SimpleLookupCircuitTest, Verify);
-  FRIEND_TEST(SimpleLookupV1CircuitTest, Verify);
-  template <typename>
-  FRIEND_TEST(ShuffleCircuitTest, Verify);
-
   bool VerifyProofForTesting(
       const VerifyingKey<F, Commitment>& vkey,
       const std::vector<std::vector<Evals>>& instance_columns_vec,
