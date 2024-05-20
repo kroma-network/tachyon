eddbc5b6ca70a09f02b441c11f2cf7c24ecb0e4d
Insun35 <inseonyu7@gmail.com>
2024-05-20 09:37:03 +0900

    fix(zk): change commit input to `Evals` to use `CommitLagrange`

--- tachyon/zk/plonk/halo2/verifier.h
+++ tachyon/zk/plonk/halo2/verifier.h
@@ -175,7 +175,7 @@ class Verifier : public VerifierBase<PCS> {
     return base::Map(columns, [this](const Evals& column) {
       std::vector<F> expanded_evals = column.evaluations();
       expanded_evals.resize(this->pcs_.N());
-      return this->Commit(expanded_evals);
+      return this->Commit(Evals(expanded_evals));
     });
   }
 
