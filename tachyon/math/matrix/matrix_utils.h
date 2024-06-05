fabcbe17fab761ac22f025f31d023a2d0992b890
Ryan Kim <chokobole33@gmail.com>
2024-06-05 17:26:56 +0900

    feat(math): implement `PackRow(Horizontally|Vertically)`
    See https://github.com/Plonky3/Plonky3/blob/f535642f5bea6a2bfded8c4e51b1f2960b1f6da7/matrix/src/lib.rs#L101-L121

--- tachyon/math/matrix/matrix_utils.h
+++ tachyon/math/matrix/matrix_utils.h
@@ -1,7 +1,12 @@
 #ifndef TACHYON_MATH_MATRIX_MATRIX_UTILS_H_
 #define TACHYON_MATH_MATRIX_MATRIX_UTILS_H_
 
+#include <vector>
+
 #include "Eigen/Core"
+
+#include "tachyon/base/containers/container_util.h"
+#include "tachyon/math/finite_fields/packed_prime_field_traits_forward.h"
 
 namespace tachyon::math {
 
@@ -38,6 +43,35 @@ MakeCirculant(const Eigen::MatrixBase<ArgType>& arg) {
   typedef typename CirculantHelper<ArgType>::MatrixType MatrixType;
   return MatrixType::NullaryExpr(arg.size(), arg.size(),
                                  CirculantFunctor<ArgType>(arg.derived()));
+}
+
+template <typename PackedPrimeField, typename Derived, typename PrimeField>
+std::vector<PackedPrimeField> PackRowHorizontally(
+    const Eigen::MatrixBase<Derived>& matrix, size_t row,
+    std::vector<PrimeField>& remaining_values) {
+  size_t num_packed = matrix.cols() / PackedPrimeField::N;
+  size_t remaining_start_idx = num_packed * PackedPrimeField::N;
+  remaining_values =
+      base::CreateVector(matrix.cols() - remaining_start_idx,
+                         [row, remaining_start_idx, &matrix](size_t col) {
+                           return matrix(row, remaining_start_idx + col);
+                         });
+
+  return base::CreateVector(num_packed, [row, &matrix](size_t col) {
+    return PackedPrimeField::From([row, col, &matrix](size_t i) {
+      return matrix(row, PackedPrimeField::N * col + i);
+    });
+  });
+}
+
+template <typename PackedPrimeField, typename Derived>
+std::vector<PackedPrimeField> PackRowVertically(
+    const Eigen::MatrixBase<Derived>& matrix, size_t row) {
+  return base::CreateVector(matrix.cols(), [row, &matrix](size_t col) {
+    return PackedPrimeField::From([row, col, &matrix](size_t i) {
+      return matrix((row + i) % matrix.rows(), col);
+    });
+  });
 }
 
 }  // namespace tachyon::math
