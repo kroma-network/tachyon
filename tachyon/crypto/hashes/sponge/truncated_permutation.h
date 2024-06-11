57305317f08426036c895fd703b9d267d360c5db
Ryan Kim <chokobole33@gmail.com>
2024-06-11 17:30:07 +0900

    feat(crypto): add `TruncatedPermutation`
    See
    https://github.com/Plonky3/Plonky3/blob/fde81db95b8eeb39e07890f4099a7d59daeec52f/symmetric/src/compression.rs#L6-L44.

---
+++ tachyon/crypto/hashes/sponge/truncated_permutation.h
@@ -0,0 +1,54 @@
+// Copyright (c) 2022 The Plonky3 Authors
+// Use of this source code is governed by a MIT/Apache-2.0 style license that
+// can be found in the LICENSE-MIT.plonky3 and the LICENCE-APACHE.plonky3
+// file.
+
+#ifndef TACHYON_CRYPTO_HASHES_SPONGE_TRUNCATED_PERMUTATION_H_
+#define TACHYON_CRYPTO_HASHES_SPONGE_TRUNCATED_PERMUTATION_H_
+
+#include <stddef.h>
+
+#include <array>
+#include <utility>
+
+#include "tachyon/crypto/hashes/compressor.h"
+#include "tachyon/crypto/hashes/sponge/sponge.h"
+
+namespace tachyon::crypto {
+
+template <typename Derived, size_t Chunk, size_t N>
+class TruncatedPermutation final
+    : public Compressor<TruncatedPermutation<Derived, Chunk, N>> {
+ public:
+  using F = typename CryptographicSpongeTraits<Derived>::F;
+
+  explicit TruncatedPermutation(const Derived& derived) : derived_(derived) {}
+  explicit TruncatedPermutation(Derived&& derived)
+      : derived_(std::move(derived)) {}
+
+ private:
+  friend class Compressor<TruncatedPermutation<Derived, Chunk, N>>;
+
+  template <typename T>
+  std::array<F, Chunk> DoCompress(const T& input) const {
+    auto& state = derived_.state;
+    size_t idx = 0;
+    for (size_t i = 0; i < N; ++i) {
+      for (size_t j = 0; j < Chunk; ++j) {
+        state[idx++] = input[i][j];
+      }
+    }
+    derived_.Permute();
+    std::array<F, Chunk> ret;
+    for (size_t i = 0; i < Chunk; ++i) {
+      ret[i] = state[i];
+    }
+    return ret;
+  }
+
+  mutable Derived derived_;
+};
+
+}  // namespace tachyon::crypto
+
+#endif  // TACHYON_CRYPTO_HASHES_SPONGE_TRUNCATED_PERMUTATION_H_
