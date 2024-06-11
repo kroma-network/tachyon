4f16e7a1867f4ed213c3cd3c05377703dc8a5984
Ryan Kim <chokobole33@gmail.com>
2024-06-11 17:07:21 +0900

    feat(crypto): add `PaddingFreeSponge`
    See
    https://github.com/Plonky3/Plonky3/blob/fde81db95b8eeb39e07890f4099a7d59daeec52f/symmetric/src/sponge.rs#L10-L44.

---
+++ tachyon/crypto/hashes/sponge/padding_free_sponge.h
@@ -0,0 +1,55 @@
+// Copyright (c) 2022 The Plonky3 Authors
+// Use of this source code is governed by a MIT/Apache-2.0 style license that
+// can be found in the LICENSE-MIT.plonky3 and the LICENCE-APACHE.plonky3
+// file.
+
+#ifndef TACHYON_CRYPTO_HASHES_SPONGE_PADDING_FREE_SPONGE_H_
+#define TACHYON_CRYPTO_HASHES_SPONGE_PADDING_FREE_SPONGE_H_
+
+#include <stddef.h>
+
+#include <array>
+#include <utility>
+
+#include "tachyon/crypto/hashes/hasher.h"
+#include "tachyon/crypto/hashes/sponge/sponge.h"
+
+namespace tachyon::crypto {
+
+template <typename Derived, size_t Rate, size_t Out>
+class PaddingFreeSponge final
+    : public Hasher<PaddingFreeSponge<Derived, Rate, Out>> {
+ public:
+  using F = typename CryptographicSpongeTraits<Derived>::F;
+
+  explicit PaddingFreeSponge(const Derived& derived) : derived_(derived) {}
+  explicit PaddingFreeSponge(Derived&& derived)
+      : derived_(std::move(derived)) {}
+
+ private:
+  friend class Hasher<PaddingFreeSponge<Derived, Rate, Out>>;
+
+  template <typename T>
+  std::array<F, Out> DoHash(const T& input) const {
+    auto& state = derived_.state;
+    for (size_t i = 0; i < std::size(input); i += Rate) {
+      for (size_t j = 0; j < Rate; ++j) {
+        if (i + j < std::size(input)) {
+          state[j] = input[i + j];
+        }
+      }
+      derived_.Permute();
+    }
+    std::array<F, Out> ret;
+    for (size_t i = 0; i < Out; ++i) {
+      ret[i] = state[i];
+    }
+    return ret;
+  }
+
+  mutable Derived derived_;
+};
+
+}  // namespace tachyon::crypto
+
+#endif  // TACHYON_CRYPTO_HASHES_SPONGE_PADDING_FREE_SPONGE_H_
