a648e58c30748f95cca678ad1d3fb32ed980633c
Wonyong Kim <chokobole33@gmail.com>
2024-06-23 16:25:33 +0900

    refac(c/zk): use `c::base::xxx_cast()` for bn254 constraint system

--- tachyon/c/zk/plonk/constraint_system/bn254_constraint_system.cc
+++ tachyon/c/zk/plonk/constraint_system/bn254_constraint_system.cc
@@ -2,23 +2,20 @@
 
 #include <vector>
 
-#include "tachyon/math/elliptic_curves/bn/bn254/fr.h"
-#include "tachyon/zk/plonk/constraint_system/constraint_system.h"
+#include "tachyon/c/zk/plonk/constraint_system/bn254_constraint_system_type_traits.h"
 
 using namespace tachyon;
 
-using CS = zk::plonk::ConstraintSystem<math::bn254::Fr>;
-
 uint32_t tachyon_bn254_plonk_constraint_system_compute_blinding_factors(
     const tachyon_bn254_plonk_constraint_system* cs) {
-  return reinterpret_cast<const CS*>(cs)->ComputeBlindingFactors();
+  return c::base::native_cast(cs)->ComputeBlindingFactors();
 }
 
 void tachyon_bn254_plonk_constraint_system_get_advice_column_phases(
     const tachyon_bn254_plonk_constraint_system* cs, tachyon_phase* phases,
     size_t* phases_len) {
   const std::vector<zk::plonk::Phase>& cpp_phases =
-      reinterpret_cast<const CS*>(cs)->advice_column_phases();
+      c::base::native_cast(cs)->advice_column_phases();
   *phases_len = cpp_phases.size();
   if (phases == nullptr) return;
   for (size_t i = 0; i < cpp_phases.size(); ++i) {
@@ -30,7 +27,7 @@ void tachyon_bn254_plonk_constraint_system_get_challenge_phases(
     const tachyon_bn254_plonk_constraint_system* cs, tachyon_phase* phases,
     size_t* phases_len) {
   const std::vector<zk::plonk::Phase>& cpp_phases =
-      reinterpret_cast<const CS*>(cs)->challenge_phases();
+      c::base::native_cast(cs)->challenge_phases();
   *phases_len = cpp_phases.size();
   if (phases == nullptr) return;
   for (size_t i = 0; i < cpp_phases.size(); ++i) {
@@ -42,7 +39,7 @@ void tachyon_bn254_plonk_constraint_system_get_phases(
     const tachyon_bn254_plonk_constraint_system* cs, tachyon_phase* phases,
     size_t* phases_len) {
   std::vector<zk::plonk::Phase> cpp_phases =
-      reinterpret_cast<const CS*>(cs)->GetPhases();
+      c::base::native_cast(cs)->GetPhases();
   *phases_len = cpp_phases.size();
   if (phases == nullptr) return;
   for (size_t i = 0; i < cpp_phases.size(); ++i) {
@@ -52,29 +49,29 @@ void tachyon_bn254_plonk_constraint_system_get_phases(
 
 size_t tachyon_bn254_plonk_constraint_system_get_num_fixed_columns(
     const tachyon_bn254_plonk_constraint_system* cs) {
-  return reinterpret_cast<const CS*>(cs)->num_fixed_columns();
+  return c::base::native_cast(cs)->num_fixed_columns();
 }
 
 size_t tachyon_bn254_plonk_constraint_system_get_num_instance_columns(
     const tachyon_bn254_plonk_constraint_system* cs) {
-  return reinterpret_cast<const CS*>(cs)->num_instance_columns();
+  return c::base::native_cast(cs)->num_instance_columns();
 }
 
 size_t tachyon_bn254_plonk_constraint_system_get_num_advice_columns(
     const tachyon_bn254_plonk_constraint_system* cs) {
-  return reinterpret_cast<const CS*>(cs)->num_advice_columns();
+  return c::base::native_cast(cs)->num_advice_columns();
 }
 
 size_t tachyon_bn254_plonk_constraint_system_get_num_challenges(
     const tachyon_bn254_plonk_constraint_system* cs) {
-  return reinterpret_cast<const CS*>(cs)->num_challenges();
+  return c::base::native_cast(cs)->num_challenges();
 }
 
 void tachyon_bn254_plonk_constraint_system_get_constants(
     const tachyon_bn254_plonk_constraint_system* cs,
     tachyon_fixed_column_key* constants, size_t* constants_len) {
   const std::vector<zk::plonk::FixedColumnKey>& cpp_constants =
-      reinterpret_cast<const CS*>(cs)->constants();
+      c::base::native_cast(cs)->constants();
   *constants_len = cpp_constants.size();
   if (constants == nullptr) return;
   for (size_t i = 0; i < cpp_constants.size(); ++i) {
