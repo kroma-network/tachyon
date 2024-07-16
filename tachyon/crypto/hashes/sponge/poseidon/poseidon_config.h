59fcfb76e98ee4c059cd01a3ade293d46985bd5e
Ryan Kim <chokobole33@gmail.com>
2024-07-16 22:53:11 +0900

    feat(crypto): implement computing optimized ARK
    See
    https://github.com/privacy-scaling-explorations/poseidon/blob/d29f35d9744e0c58b63679a15730ff2fab470ef2/src/spec.rs#L318-L372.

--- tachyon/crypto/hashes/sponge/poseidon/poseidon_config.h
+++ tachyon/crypto/hashes/sponge/poseidon/poseidon_config.h
@@ -3,12 +3,18 @@
 // can be found in the LICENSE-MIT.arkworks and the LICENCE-APACHE.arkworks
 // file.
 
+// Copyright 2022 Ethereum Foundation
+// Use of this source code is governed by a MIT/Apache-2.0 style license that
+// can be found in the LICENSE-MIT.EF and the LICENCE-APACHE.EF
+// file.
+
 #ifndef TACHYON_CRYPTO_HASHES_SPONGE_POSEIDON_POSEIDON_CONFIG_H_
 #define TACHYON_CRYPTO_HASHES_SPONGE_POSEIDON_POSEIDON_CONFIG_H_
 
 #include <utility>
 
 #include "absl/types/span.h"
+#include "third_party/eigen3/Eigen/LU"
 
 #include "tachyon/base/buffer/copyable.h"
 #include "tachyon/base/logging.h"
@@ -16,6 +22,7 @@
 #include "tachyon/base/ranges/algorithm.h"
 #include "tachyon/crypto/hashes/sponge/poseidon/poseidon_config_base.h"
 #include "tachyon/crypto/hashes/sponge/poseidon/poseidon_config_entry.h"
+#include "tachyon/math/finite_fields/finite_field_traits.h"
 
 namespace tachyon {
 namespace crypto {
@@ -81,6 +88,7 @@ struct PoseidonConfig : public PoseidonConfigBase<F> {
     PoseidonConfig ret = it->template ToPoseidonConfig<F>();
     FindPoseidonARKAndMDS<F>(it->template ToPoseidonGrainLFSRConfig<F>(),
                              it->skip_matrices, ret.ark, ret.mds);
+    OptimizeARK(ret.full_rounds, ret.partial_rounds, ret.mds, ret.ark);
     return ret;
   }
 
@@ -93,6 +101,7 @@ struct PoseidonConfig : public PoseidonConfigBase<F> {
     PoseidonConfig ret = config_entry.ToPoseidonConfig<F>();
     FindPoseidonARKAndMDS<F>(config_entry.ToPoseidonGrainLFSRConfig<F>(),
                              skip_matrices, ret.ark, ret.mds);
+    OptimizeARK(full_rounds, partial_rounds, ret.mds, ret.ark);
     return ret;
   }
 
@@ -108,6 +117,38 @@ struct PoseidonConfig : public PoseidonConfigBase<F> {
   bool operator!=(const PoseidonConfig& other) const {
     return !operator==(other);
   }
+
+ private:
+  constexpr static void OptimizeARK(size_t full_rounds, size_t partial_rounds,
+                                    const math::Matrix<F>& mds,
+                                    math::Matrix<F>& ark) {
+    math::Matrix<F> mds_inverse = mds.inverse();
+    math::Matrix<F> optimized_ark = {ark.rows(), ark.cols()};
+    optimized_ark.row(0) = ark.row(0);
+    size_t full_rounds_over_2 = full_rounds / 2;
+    for (size_t i = 1; i < full_rounds_over_2; ++i) {
+      optimized_ark.row(i) = mds_inverse * ark.row(i).transpose();
+    }
+
+    math::RowVector<F> acc = ark.row(full_rounds_over_2 + partial_rounds);
+    for (size_t i = full_rounds_over_2; i < full_rounds_over_2 + partial_rounds;
+         ++i) {
+      math::RowVector<F> tmp = mds_inverse * acc.transpose();
+      optimized_ark.row(full_rounds + partial_rounds - i).setZero();
+      optimized_ark.row(full_rounds + partial_rounds - i)[0] = tmp[0];
+
+      tmp[0] = F::Zero();
+      acc = tmp + ark.row(full_rounds + partial_rounds - i - 1);
+    }
+    optimized_ark.row(full_rounds_over_2) = mds_inverse * acc.transpose();
+
+    for (size_t i = full_rounds_over_2 + partial_rounds + 1;
+         i < partial_rounds + full_rounds; ++i) {
+      optimized_ark.row(i) = mds_inverse * ark.row(i).transpose();
+    }
+  }
+  // TODO(chokobole): This will be uncommented!
+  // ark = std::move(optimized_ark);
 };
 
 template <typename F>
@@ -130,7 +171,7 @@ class Copyable<crypto::PoseidonConfig<F>> {
  public:
   static bool WriteTo(const crypto::PoseidonConfig<F>& config, Buffer* buffer) {
     return Copyable<crypto::PoseidonConfigBase<F>>::WriteTo(config, buffer) &&
-           buffer->Write(config.mds);
+           buffer->WriteMany(config.mds);
   }
 
   static bool ReadFrom(const ReadOnlyBuffer& buffer,
