027a42cbdcd67563d3af50b2d3a97275e545d697
Ryan Kim <chokobole33@gmail.com>
2024-07-17 17:59:35 +0900

    feat(crypto): implement computing sparse mds matrices
    See
    https://github.com/privacy-scaling-explorations/poseidon/blob/d29f35d9744e0c58b63679a15730ff2fab470ef2/src/spec.rs#L374-L390
    and
    https://github.com/privacy-scaling-explorations/poseidon/blob/d29f35d9744e0c58b63679a15730ff2fab470ef2/src/spec.rs#L194-L227.

--- tachyon/crypto/hashes/sponge/poseidon/poseidon_config.h
+++ tachyon/crypto/hashes/sponge/poseidon/poseidon_config.h
@@ -11,7 +11,9 @@
 #ifndef TACHYON_CRYPTO_HASHES_SPONGE_POSEIDON_POSEIDON_CONFIG_H_
 #define TACHYON_CRYPTO_HASHES_SPONGE_POSEIDON_POSEIDON_CONFIG_H_
 
+#include <algorithm>
 #include <utility>
+#include <vector>
 
 #include "absl/types/span.h"
 #include "third_party/eigen3/Eigen/LU"
@@ -22,6 +24,7 @@
 #include "tachyon/base/ranges/algorithm.h"
 #include "tachyon/crypto/hashes/sponge/poseidon/poseidon_config_base.h"
 #include "tachyon/crypto/hashes/sponge/poseidon/poseidon_config_entry.h"
+#include "tachyon/crypto/hashes/sponge/poseidon/sparse_mds_matrix.h"
 #include "tachyon/math/finite_fields/finite_field_traits.h"
 
 namespace tachyon {
@@ -69,11 +72,26 @@ struct PoseidonConfig : public PoseidonConfigBase<F> {
   // Maximally Distance Separating (MDS) Matrix.
   math::Matrix<F> mds;
 
+  // See
+  // https://github.com/privacy-scaling-explorations/poseidon/blob/d29f35d9744e0c58b63679a15730ff2fab470ef2/src/spec.rs#L122-L123
+  math::Matrix<F> pre_sparse_mds;
+  std::vector<SparseMDSMatrix<F>> sparse_mds_matrices;
+
   PoseidonConfig() = default;
-  PoseidonConfig(const PoseidonConfigBase<F>& base, const math::Matrix<F>& mds)
-      : PoseidonConfigBase<F>(base), mds(mds) {}
-  PoseidonConfig(PoseidonConfigBase<F>&& base, math::Matrix<F>&& mds)
-      : PoseidonConfigBase<F>(std::move(base)), mds(std::move(mds)) {}
+  PoseidonConfig(const PoseidonConfigBase<F>& base, const math::Matrix<F>& mds,
+                 const math::Matrix<F>& pre_sparse_mds,
+                 const std::vector<SparseMDSMatrix<F>>& sparse_mds_matrices)
+      : PoseidonConfigBase<F>(base),
+        mds(mds),
+        pre_sparse_mds(pre_sparse_mds),
+        sparse_mds_matrices(sparse_mds_matrices) {}
+  PoseidonConfig(PoseidonConfigBase<F>&& base, math::Matrix<F>&& mds,
+                 math::Matrix<F>&& pre_sparse_mds,
+                 std::vector<SparseMDSMatrix<F>>&& sparse_mds_matrices)
+      : PoseidonConfigBase<F>(std::move(base)),
+        mds(std::move(mds)),
+        pre_sparse_mds(std::move(pre_sparse_mds)),
+        sparse_mds_matrices(std::move(sparse_mds_matrices)) {}
 
   static PoseidonConfig CreateDefault(size_t rate, bool optimized_for_weights) {
     absl::Span<const PoseidonConfigEntry> param_set =
@@ -89,6 +107,8 @@ struct PoseidonConfig : public PoseidonConfigBase<F> {
     FindPoseidonARKAndMDS<F>(it->template ToPoseidonGrainLFSRConfig<F>(),
                              it->skip_matrices, ret.ark, ret.mds);
     OptimizeARK(ret.full_rounds, ret.partial_rounds, ret.mds, ret.ark);
+    ComputeSparseMatrices(ret.partial_rounds, ret.mds, ret.pre_sparse_mds,
+                          ret.sparse_mds_matrices);
     return ret;
   }
 
@@ -102,17 +122,33 @@ struct PoseidonConfig : public PoseidonConfigBase<F> {
     FindPoseidonARKAndMDS<F>(config_entry.ToPoseidonGrainLFSRConfig<F>(),
                              skip_matrices, ret.ark, ret.mds);
     OptimizeARK(full_rounds, partial_rounds, ret.mds, ret.ark);
+    ComputeSparseMatrices(partial_rounds, ret.mds, ret.pre_sparse_mds,
+                          ret.sparse_mds_matrices);
     return ret;
   }
 
   bool IsValid() const override {
-    return PoseidonConfigBase<F>::IsValid() &&
-           static_cast<size_t>(mds.rows()) == this->rate + this->capacity &&
-           static_cast<size_t>(mds.cols()) == this->rate + this->capacity;
+    bool ret = PoseidonConfigBase<F>::IsValid() &&
+               static_cast<size_t>(mds.rows()) == this->rate + this->capacity &&
+               static_cast<size_t>(mds.cols()) == this->rate + this->capacity &&
+               static_cast<size_t>(pre_sparse_mds.rows()) ==
+                   this->rate + this->capacity &&
+               static_cast<size_t>(pre_sparse_mds.cols()) ==
+                   this->rate + this->capacity &&
+               sparse_mds_matrices.size() == this->partial_rounds;
+    if (!ret) return false;
+
+    for (const SparseMDSMatrix<F>& sparse_mds_matrix : sparse_mds_matrices) {
+      if (sparse_mds_matrix.row().size() != mds.cols()) return false;
+      if (sparse_mds_matrix.col_hat().size() != mds.rows() - 1) return false;
+    }
+    return true;
   }
 
   bool operator==(const PoseidonConfig& other) const {
-    return PoseidonConfigBase<F>::operator==(other) && mds == other.mds;
+    return PoseidonConfigBase<F>::operator==(other) && mds == other.mds &&
+           pre_sparse_mds == other.pre_sparse_mds &&
+           sparse_mds_matrices == other.sparse_mds_matrices;
   }
   bool operator!=(const PoseidonConfig& other) const {
     return !operator==(other);
@@ -146,9 +182,53 @@ struct PoseidonConfig : public PoseidonConfigBase<F> {
          i < partial_rounds + full_rounds; ++i) {
       optimized_ark.row(i) = mds_inverse * ark.row(i).transpose();
     }
+    // TODO(chokobole): This will be uncommented!
+    // ark = std::move(optimized_ark);
   }
-  // TODO(chokobole): This will be uncommented!
-  // ark = std::move(optimized_ark);
+
+  constexpr static void ComputeSparseMatrices(
+      size_t partial_rounds, const math::Matrix<F>& mds,
+      math::Matrix<F>& pre_sparse_mds,
+      std::vector<SparseMDSMatrix<F>>& sparse_matrices) {
+    math::Matrix<F> mds_transpose = mds.transpose();
+    math::Matrix<F> acc = mds_transpose;
+    sparse_matrices =
+        base::CreateVector(partial_rounds, [&mds_transpose, &acc]() {
+          math::Matrix<F> m_prime;
+          SparseMDSMatrix<F> m_prime_prime;
+          Factorize(acc, m_prime, m_prime_prime);
+          acc = mds_transpose * m_prime;
+          return m_prime_prime;
+        });
+    std::reverse(sparse_matrices.begin(), sparse_matrices.end());
+    pre_sparse_mds = acc.transpose();
+  }
+
+  // See Section B in Supplementary Material in
+  // https://eprint.iacr.org/2019/458.pdf.
+  // Factorizes an MDS matrix |M| into |M'| and |M''| where |M = M' * M''|.
+  // The |M'| matrix contributes to the accumulator of the process, and the
+  // resulting |M''| matrix is sparse.
+  constexpr static void Factorize(const math::Matrix<F>& mds,
+                                  math::Matrix<F>& m_prime,
+                                  SparseMDSMatrix<F>& m_prime_prime) {
+    // |(r - 1 * r - 1)| sized MDS matrix called |m_hat| constructs a matrix
+    // in the form |[[1 | 0], [0 | m_hat]]|, where r is rate.
+    Eigen::Block<const math::Matrix<F>> m_hat =
+        mds.block(1, 1, mds.rows() - 1, mds.cols() - 1);
+    m_prime = math::Matrix<F>::Identity(mds.rows(), mds.cols());
+    m_prime.block(1, 1, mds.rows() - 1, mds.cols() - 1) = m_hat;
+
+    // |(r - 1)| sized vector called |w_hat| is denoted as
+    // |[[m₀,₀ | m₀,ᵢ], [w_hat | identity]]|, where r is rate.
+    auto w_hat = m_hat.inverse() * mds.block(1, 0, mds.rows() - 1, 1);
+    math::Matrix<F> m_prime_prime_mds =
+        math::Matrix<F>::Identity(mds.rows(), mds.cols());
+    m_prime_prime_mds.row(0) = mds.row(0);
+    m_prime_prime_mds.block(1, 0, w_hat.rows(), 1) = w_hat;
+    m_prime_prime =
+        SparseMDSMatrix<F>::FromMDSMatrix(m_prime_prime_mds.transpose());
+  }
 };
 
 template <typename F>
@@ -165,31 +245,35 @@ PoseidonConfig<F> PoseidonConfigEntry::ToPoseidonConfig() const {
 }  // namespace crypto
 
 namespace base {
-
 template <typename F>
 class Copyable<crypto::PoseidonConfig<F>> {
  public:
   static bool WriteTo(const crypto::PoseidonConfig<F>& config, Buffer* buffer) {
     return Copyable<crypto::PoseidonConfigBase<F>>::WriteTo(config, buffer) &&
-           buffer->WriteMany(config.mds);
+           buffer->WriteMany(config.mds, config.pre_sparse_mds,
+                             config.sparse_mds_matrices);
   }
 
   static bool ReadFrom(const ReadOnlyBuffer& buffer,
                        crypto::PoseidonConfig<F>* config) {
     crypto::PoseidonConfigBase<F> base;
     math::Matrix<F> mds;
-    if (!buffer.ReadMany(&base, &mds)) {
+    math::Matrix<F> pre_sparse_mds;
+    std::vector<crypto::SparseMDSMatrix<F>> sparse_mds_matrices;
+    if (!buffer.ReadMany(&base, &mds, &pre_sparse_mds, &sparse_mds_matrices)) {
       return false;
     }
 
-    *config = {std::move(base), std::move(mds)};
+    *config = {std::move(base), std::move(mds), std::move(pre_sparse_mds),
+               std::move(sparse_mds_matrices)};
     return true;
   }
 
   static size_t EstimateSize(const crypto::PoseidonConfig<F>& config) {
     const crypto::PoseidonConfigBase<F>& base =
         static_cast<const crypto::PoseidonConfigBase<F>&>(config);
-    return base::EstimateSize(base, config.mds);
+    return base::EstimateSize(base, config.mds, config.pre_sparse_mds,
+                              config.sparse_mds_matrices);
   }
 };
 
