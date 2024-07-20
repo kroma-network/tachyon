713015bae3c7f74b7b0cbf3216427e784f314b5f
ashjeong <ashjeong@umich.edu>
2024-07-20 16:20:51 +0900

    feat(math): impl `ExtendedPackedFieldTraits`

---
+++ tachyon/math/finite_fields/extended_packed_field_traits_forward.h
@@ -0,0 +1,11 @@
+#ifndef TACHYON_MATH_FINITE_FIELDS_EXTENDED_PACKED_FIELD_TRAITS_FORWARD_H_
+#define TACHYON_MATH_FINITE_FIELDS_EXTENDED_PACKED_FIELD_TRAITS_FORWARD_H_
+
+namespace tachyon::math {
+
+template <typename T, typename SFINAE = void>
+struct ExtendedPackedFieldTraits;
+
+}  // namespace tachyon::math
+
+#endif  // TACHYON_MATH_FINITE_FIELDS_EXTENDED_PACKED_FIELD_TRAITS_FORWARD_H_
