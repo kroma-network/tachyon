e2e65cc7638baf1912a1df297e9d37e267ee6e3f
ashjeong <ashjeong@umich.edu>
2024-07-25 15:25:42 +0900

    feat(crypto): add two adic fri proof structs
    See [here](https://github.com/Plonky3/Plonky3/blob/eeb4e37/fri/src/proof.rs) and [here](https://github.com/Plonky3/Plonky3/blob/eeb4e37/fri/src/two_adic_pcs.rs#L49-L52).

---
+++ tachyon/crypto/commitments/fri/two_adic_fri_proof.h
@@ -0,0 +1,54 @@
+// Copyright (c) 2022 The Plonky3 Authors
+// Use of this source code is governed by a MIT/Apache-2.0 style license that
+// can be found in the LICENSE-MIT.plonky3 and the LICENCE-APACHE.plonky3
+// file.
+
+#ifndef TACHYON_CRYPTO_COMMITMENTS_FRI_TWO_ADIC_FRI_PROOF_H_
+#define TACHYON_CRYPTO_COMMITMENTS_FRI_TWO_ADIC_FRI_PROOF_H_
+
+#include <vector>
+
+#include "tachyon/crypto/commitments/mixed_matrix_commitment_scheme_traits_forward.h"
+
+namespace tachyon::crypto {
+
+template <typename MMCS>
+struct BatchOpening {
+  std::vector<std::vector<typename MMCS::Field>> opened_values;
+  typename MMCS::Proof opening_proof;
+};
+
+template <typename MMCS>
+struct CommitPhaseResult {
+  std::vector<typename MMCS::Commitment> commits;
+  std::vector<typename MMCS::ProverData> data;
+  typename MMCS::Field final_eval;
+};
+
+template <typename MMCS>
+struct CommitPhaseProofStep {
+  // The opening of the commit phase codeword at the sibling location.
+  typename MMCS::Field sibling_value;
+  typename MMCS::Proof opening_proof;
+};
+
+// Note(ashjeong): |InputProof| is usually a vector of |BatchOpenings|
+template <typename MMCS, typename InputProof>
+struct QueryProof {
+  InputProof input_proof;
+  // For each commit phase commitment, this contains openings of a commit phase
+  // codeword at the queried location, along with an opening proof.
+  std::vector<CommitPhaseProofStep<MMCS>> commit_phase_openings;
+};
+
+template <typename MMCS, typename InputProof, typename Witness>
+struct TwoAdicFriProof {
+  std::vector<typename MMCS::Commitment> commit_phase_commits;
+  std::vector<QueryProof<MMCS, InputProof>> query_proofs;
+  typename MMCS::Field final_eval;
+  Witness pow_witness;
+};
+
+}  // namespace tachyon::crypto
+
+#endif  // TACHYON_CRYPTO_COMMITMENTS_FRI_TWO_ADIC_FRI_PROOF_H_
