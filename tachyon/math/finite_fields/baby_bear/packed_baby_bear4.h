8f108cca1769d3399bcb9cd70e8cd68a1aa7ef69
ashjeong <ashjeong@umich.edu>
2024-07-29 13:55:48 +0900

    feat(math): prep `ExtendedPackedFieldtraits` for `GetExtendedPackedPowers()`

--- tachyon/math/finite_fields/baby_bear/packed_baby_bear4.h
+++ tachyon/math/finite_fields/baby_bear/packed_baby_bear4.h
@@ -1,15 +1,16 @@
 #ifndef TACHYON_MATH_FINITE_FIELDS_BABY_BEAR_PACKED_BABY_BEAR4_H_
 #define TACHYON_MATH_FINITE_FIELDS_BABY_BEAR_PACKED_BABY_BEAR4_H_
 
-#include "tachyon/math/finite_fields/baby_bear/internal/baby_bear4.h"
 #include "tachyon/math/finite_fields/baby_bear/internal/packed_baby_bear4.h"
-#include "tachyon/math/finite_fields/extended_packed_field_traits_forward.h"
 
 namespace tachyon::math {
 
 template <>
 struct ExtendedPackedFieldTraits<PackedBabyBear4> {
-  using PackedField = PackedBabyBear;
+  constexpr static bool kIsExtendedPackedField = true;
+  // Note(ashjeong): |ExtendedPackedField| is defaulted as itself. See
+  // extension_field_base.h to see how it is used.
+  using ExtendedPackedField = PackedBabyBear4;
 };
 
 }  // namespace tachyon::math
--- tachyon/math/finite_fields/koala_bear/koala_bear4.h
+++ tachyon/math/finite_fields/koala_bear/koala_bear4.h
@@ -1,14 +1,14 @@
 #ifndef TACHYON_MATH_FINITE_FIELDS_KOALA_BEAR_KOALA_BEAR4_H_
 #define TACHYON_MATH_FINITE_FIELDS_KOALA_BEAR_KOALA_BEAR4_H_
 
-#include "tachyon/math/finite_fields/baby_bear/internal/koala_bear.h"
-#include "tachyon/math/finite_fields/baby_bear/internal/packed_koala_bear.h"
-#include "tachyon/math/finite_fields/extended_packed_field_traits_forward.h"
+#include "tachyon/math/finite_fields/baby_bear/internal/koala_bear4.h"
+#include "tachyon/math/finite_fields/baby_bear/internal/packed_koala_bear4.h"
 
 namespace tachyon::math {
 
 template <>
 struct ExtendedPackedFieldTraits<KoalaBear4> {
+  constexpr static bool kIsExtendedPackedField = false;
   using ExtendedPackedField = PackedKoalaBear4;
 };
 
