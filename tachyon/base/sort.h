0f5b888dda78cf59278db9f1f0fb1bc968207b73
Ryan Kim <chokobole33@gmail.com>
2024-08-05 17:54:03 +0900

    feat: wrap `powersort` with `base::StableSort()`
    See https://www.wild-inter.net/posts/powersort-in-python-3.11.

--- tachyon/base/sort.h
+++ tachyon/base/sort.h
@@ -2,6 +2,7 @@
 #define TACHYON_BASE_SORT_H_
 
 #include "third_party/pdqsort/include/pdqsort.h"
+#include "third_party/powersort/include/sorts/powersort.h"
 
 namespace tachyon::base {
 
@@ -13,6 +14,13 @@ void UnstableSort(Iter begin, Iter end) {
 template <typename Iter, typename Compare>
 void UnstableSort(Iter begin, Iter end, Compare compare) {
   return pdqsort(begin, end, compare);
+}
+
+// TODO(chokobole): Add StableSort() with compare version.
+template <typename Iter>
+void StableSort(Iter begin, Iter end) {
+  algorithms::powersort<Iter> sort;
+  sort.sort(begin, end);
 }
 
 }  // namespace tachyon::base
