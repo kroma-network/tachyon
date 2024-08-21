b183939456d3dd9799573ba23a7d5e222df3f0bb
GideokKim <ikid4127@naver.com>
2024-08-21 16:52:22 +0900

    feat(crypto): apply icicle poseidon2 to tachyon poseidon2

--- tachyon/crypto/hashes/sponge/poseidon2/poseidon2.h
+++ tachyon/crypto/hashes/sponge/poseidon2/poseidon2.h
@@ -6,6 +6,7 @@
 #ifndef TACHYON_CRYPTO_HASHES_SPONGE_POSEIDON2_POSEIDON2_H_
 #define TACHYON_CRYPTO_HASHES_SPONGE_POSEIDON2_POSEIDON2_H_
 
+#include <memory>
 #include <utility>
 
 #include "tachyon/base/buffer/copyable.h"
@@ -16,6 +17,13 @@
 #include "tachyon/crypto/hashes/sponge/poseidon2/poseidon2_plonky3_internal_matrix.h"
 #include "tachyon/crypto/hashes/sponge/sponge_state.h"
 #include "tachyon/math/finite_fields/finite_field_traits.h"
+
+#if TACHYON_CUDA
+#include "tachyon/crypto/hashes/sponge/poseidon2/icicle/icicle_poseidon2.h"
+#include "tachyon/device/gpu/gpu_memory.h"
+#include "tachyon/device/gpu/scoped_mem_pool.h"
+#include "tachyon/device/gpu/scoped_stream.h"
+#endif
 
 namespace tachyon {
 namespace crypto {
@@ -35,12 +43,88 @@ struct Poseidon2Sponge final
 
   // Sponge Config
   Poseidon2Config<Params> config;
+#if TACHYON_CUDA
+  device::gpu::ScopedMemPool mem_pool_;
+  device::gpu::ScopedStream stream_;
+  std::unique_ptr<IciclePoseidon2<F>> poseidon2_gpu_;
+#endif
 
   Poseidon2Sponge() = default;
   explicit Poseidon2Sponge(const Poseidon2Config<Params>& config)
-      : config(config) {}
+      : config(config) {
+#if TACHYON_CUDA
+    SetupForGpu();
+#endif
+  }
   explicit Poseidon2Sponge(Poseidon2Config<Params>&& config)
-      : config(std::move(config)) {}
+      : config(std::move(config)) {
+#if TACHYON_CUDA
+    SetupForGpu();
+#endif
+  }
+
+#if TACHYON_CUDA
+  void SetupForGpu() {
+    if constexpr (IsIciclePoseidon2Supported<F>) {
+      if (poseidon2_gpu_) return;
+
+      gpuMemPoolProps props = {gpuMemAllocationTypePinned,
+                               gpuMemHandleTypeNone,
+                               {gpuMemLocationTypeDevice, 0}};
+      mem_pool_ = device::gpu::CreateMemPool(&props);
+
+      uint64_t mem_pool_threshold = std::numeric_limits<uint64_t>::max();
+      gpuError_t error = gpuMemPoolSetAttribute(
+          mem_pool_.get(), gpuMemPoolAttrReleaseThreshold, &mem_pool_threshold);
+      CHECK_EQ(error, gpuSuccess);
+      stream_ = device::gpu::CreateStream();
+
+      poseidon2_gpu_.reset(
+          new IciclePoseidon2<F>(mem_pool_.get(), stream_.get()));
+
+      if (config.use_plonky3_internal_matrix) {
+        math::Vector<F> internal_vector = math::Vector<F>(Params::kWidth);
+        internal_vector[0] = F(F::Config::kModulus - 2);
+        for (Eigen::Index i = 1; i < internal_vector.size(); ++i) {
+          internal_vector[i] = F(uint32_t{1} << config.internal_shifts[i - 1]);
+        }
+        absl::Span<const F> internal_vector_span =
+            absl::Span<const F>(internal_vector.data(), internal_vector.size());
+        size_t capacity =
+            Params::kFullRounds * Params::kWidth + Params::kPartialRounds;
+
+        std::vector<F> ark_vector;
+        ark_vector.reserve(capacity);
+        Eigen::Index partial_rounds_start = Params::kFullRounds / 2;
+        Eigen::Index partial_rounds_end =
+            Params::kFullRounds / 2 + Params::kPartialRounds;
+        for (Eigen::Index i = 0; i < config.ark.rows(); ++i) {
+          if (i < partial_rounds_start || i >= partial_rounds_end) {
+            for (Eigen::Index j = 0; j < config.ark.cols(); ++j) {
+              ark_vector.push_back(config.ark(i, j));
+            }
+          } else {
+            ark_vector.push_back(config.ark(i, 0));
+          }
+        }
+        absl::Span<const F> ark_span =
+            absl::Span<const F>(ark_vector.data(), ark_vector.size());
+        if (poseidon2_gpu_->Create(Params::kWidth, Params::kRate,
+                                   Params::kAlpha, Params::kPartialRounds,
+                                   Params::kFullRounds, ark_span,
+                                   internal_vector_span, Vendor::kPlonky3))
+          return;
+      } else {
+        if (poseidon2_gpu_->Load(Params::kWidth, Params::kRate,
+                                 Vendor::kHorizen))
+          return;
+      }
+
+      LOG(ERROR) << "Failed poseidon2 gpu setup";
+      poseidon2_gpu_.reset();
+    }
+  }
+#endif
 
   // PoseidonSpongeBase methods
   void Permute(SpongeState<Params>& state) const {
