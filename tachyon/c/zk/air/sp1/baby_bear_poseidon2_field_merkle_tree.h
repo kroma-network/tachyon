5de690b79d405be3208281d0ed031feba4da093f
Ryan Kim <chokobole33@gmail.com>
2024-09-23 13:19:18 +0900

    feat(c/zk): add c API to (de)serialize to/from bytes for `FieldMerkleTree`

--- tachyon/c/zk/air/sp1/baby_bear_poseidon2_field_merkle_tree.h
+++ tachyon/c/zk/air/sp1/baby_bear_poseidon2_field_merkle_tree.h
@@ -7,6 +7,9 @@
 #ifndef TACHYON_C_ZK_AIR_SP1_BABY_BEAR_POSEIDON2_FIELD_MERKLE_TREE_H_
 #define TACHYON_C_ZK_AIR_SP1_BABY_BEAR_POSEIDON2_FIELD_MERKLE_TREE_H_
 
+#include <stddef.h>
+#include <stdint.h>
+
 #include "tachyon/c/export.h"
 
 struct tachyon_sp1_baby_bear_poseidon2_field_merkle_tree {};
@@ -35,6 +38,29 @@ tachyon_sp1_baby_bear_poseidon2_field_merkle_tree_clone(
  */
 TACHYON_C_EXPORT void tachyon_sp1_baby_bear_poseidon2_field_merkle_tree_destroy(
     tachyon_sp1_baby_bear_poseidon2_field_merkle_tree* tree);
+
+/**
+ * @brief Serializes a field merkle tree to the byte array.
+ *
+ * @param tree A const pointer to the set of field merkle tree.
+ * @param data A pointer to the byte array.
+ * @param data_len A pointer to store the length of the byte array.
+ */
+TACHYON_C_EXPORT void
+tachyon_sp1_baby_bear_poseidon2_field_merkle_tree_serialize(
+    const tachyon_sp1_baby_bear_poseidon2_field_merkle_tree* tree,
+    uint8_t* data, size_t* data_len);
+
+/**
+ * @brief Deserializes a field merkle tree from the byte array.
+ *
+ * @param data A const pointer to the byte array.
+ * @param data_len The length of the byte array.
+ * @return A pointer to the deserialized field merkle tree.
+ */
+TACHYON_C_EXPORT tachyon_sp1_baby_bear_poseidon2_field_merkle_tree*
+tachyon_sp1_baby_bear_poseidon2_field_merkle_tree_deserialize(
+    const uint8_t* data, size_t data_len);
 
 #ifdef __cplusplus
 }  // extern "C"
