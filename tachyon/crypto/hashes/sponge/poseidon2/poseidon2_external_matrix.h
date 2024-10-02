c00ab0fca9856293e4d29b7cc06f4c09d4bacddd
Ryan Kim <chokobole33@gmail.com>
2024-10-02 14:19:17 +0900

    refac(crypto): rename `kWidth` to `N`

--- tachyon/crypto/hashes/sponge/poseidon2/poseidon2_external_matrix.h
+++ tachyon/crypto/hashes/sponge/poseidon2/poseidon2_external_matrix.h
@@ -17,35 +17,35 @@ class Poseidon2ExternalMatrix {
  public:
   using Field = typename Poseidon2ExternalMatrixTraits<Derived>::Field;
 
-  template <size_t kWidth>
+  template <size_t N>
   static void Apply(math::Vector<Field>& v) {
-    if constexpr (kWidth <= 1 || kWidth > 24) {
+    if constexpr (N <= 1 || N > 24) {
       NOTREACHED() << "Out of range";
-    } else if constexpr (kWidth == 2) {
+    } else if constexpr (N == 2) {
       Field sum = v[0] + v[1];
       v[0] += sum;
       v[1] += sum;
-    } else if constexpr (kWidth == 3) {
+    } else if constexpr (N == 3) {
       Field sum = v[0] + v[1] + v[2];
       v[0] += sum;
       v[1] += sum;
       v[2] += sum;
-    } else if constexpr (kWidth == 4) {
+    } else if constexpr (N == 4) {
       Derived::DoApply(v);
-    } else if constexpr (kWidth % 4 == 0) {
-      for (size_t i = 0; i < kWidth; i += 4) {
+    } else if constexpr (N % 4 == 0) {
+      for (size_t i = 0; i < N; i += 4) {
         Eigen::Block<math::Vector<Field>> block = v.block(i, 0, 4, 1);
         Derived::DoApply(block);
       }
 
       std::array<Field, 4> v_tmp = {Field::Zero()};
       for (size_t i = 0; i < 4; ++i) {
-        for (size_t j = 0; j < kWidth; j += 4) {
+        for (size_t j = 0; j < N; j += 4) {
           v_tmp[i] += v[i + j];
         }
       }
 
-      for (size_t i = 0; i < kWidth; ++i) {
+      for (size_t i = 0; i < N; ++i) {
         v[i] += v_tmp[i % 4];
       }
     } else {
@@ -53,23 +53,23 @@ class Poseidon2ExternalMatrix {
     }
   }
 
-  template <size_t kWidth>
+  template <size_t N>
   static math::Matrix<Field> Construct() {
-    if constexpr (kWidth <= 1 || kWidth > 24) {
+    if constexpr (N <= 1 || N > 24) {
       NOTREACHED() << "Out of range";
-    } else if constexpr (kWidth == 2) {
+    } else if constexpr (N == 2) {
       return math::MakeCirculant(math::Vector<Field>{{Field(2), Field(1)}});
-    } else if constexpr (kWidth == 3) {
+    } else if constexpr (N == 3) {
       return math::MakeCirculant(
           math::Vector<Field>{{Field(2), Field(1), Field(1)}});
-    } else if constexpr (kWidth == 4) {
+    } else if constexpr (N == 4) {
       return Derived::DoConstruct();
-    } else if constexpr (kWidth % 4 == 0) {
+    } else if constexpr (N % 4 == 0) {
       math::Matrix<Field> small_matrix = Derived::DoConstruct();
 
-      math::Matrix<Field> ret(kWidth, kWidth);
-      for (size_t i = 0; i < kWidth / 4; ++i) {
-        for (size_t j = 0; j < kWidth / 4; ++j) {
+      math::Matrix<Field> ret(N, N);
+      for (size_t i = 0; i < N / 4; ++i) {
+        for (size_t j = 0; j < N / 4; ++j) {
           ret.block(i * 4, j * 4, 4, 4) =
               i == j ? Field(2) * small_matrix : small_matrix;
         }
