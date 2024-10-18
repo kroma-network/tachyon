kroma-network/tachyon

commit db5ee0aef01f4c107f6c3bb04817eecde78a005d

refac: modify `CosetLDEBatch()` to accept pre-allocated memory

benchmark/fft_batch/fft_batch_runner.h
@@ -32,14 +32,20 @@ class FFTBatchRunner {
 
   math::RowMajorMatrix<F> Run(Vendor vendor, bool run_coset_lde,
                               const math::RowMajorMatrix<F>& input) {
-    math::RowMajorMatrix<F> result = input;
+    math::RowMajorMatrix<F> result;
     std::unique_ptr<Domain> domain =
         Domain::Create(static_cast<size_t>(input.rows()));
     base::TimeTicks start = base::TimeTicks::Now();
     if (run_coset_lde) {
-      result = result = domain->CosetLDEBatch(
-          result, 1, F::FromMontgomery(F::Config::kSubgroupGenerator));
+      const size_t kAddedBits = 1;
+      math::RowMajorMatrix<F> input_tmp = input;
+      result =
+          math::RowMajorMatrix<F>(input.rows() << kAddedBits, input.cols());
+      domain->CosetLDEBatch(input_tmp, kAddedBits,
+                            F::FromMontgomery(F::Config::kSubgroupGenerator),
+                            result);
     } else {
+      result = input;
       domain->FFTBatch(result);
     }
     reporter_.AddTime(vendor, base::TimeTicks::Now() - start);

tachyon/c/crypto/commitments/fri/two_adic_fri_impl.h
@@ -33,10 +33,12 @@ class TwoAdicFRIImpl
   template <typename Derived>
   absl::Span<F> CosetLDEBatch(Eigen::MatrixBase<Derived>& matrix, F shift) {
     Domain coset = this->GetNaturalDomainForDegree(matrix.rows());
-    tachyon::math::RowMajorMatrix<F> mat = coset.domain()->CosetLDEBatch(
-        matrix, this->fri_.log_blowup, shift, /*reverse_at_last=*/false);
-    absl::Span<F> ret(mat.data(), mat.size());
-    ldes_.push_back(std::move(mat));
+    tachyon::math::RowMajorMatrix<F> lde(matrix.rows() << this->fri_.log_blowup,
+                                         matrix.cols());
+    coset.domain()->CosetLDEBatch(matrix, this->fri_.log_blowup, shift, lde,
+                                  /*reverse_at_last=*/false);
+    absl::Span<F> ret(lde.data(), lde.size());
+    ldes_.push_back(std::move(lde));
     return ret;
   }
 

tachyon/crypto/commitments/fri/two_adic_fri.h
@@ -76,11 +76,16 @@ class TwoAdicFRI {
     TRACE_EVENT("ProofGeneration", "TwoAdicFRI::Commit");
     std::vector<math::RowMajorMatrix<F>> ldes =
         base::Map(cosets, [this, &matrices](size_t i, const Domain& coset) {
-          return coset.domain()->CosetLDEBatch(
-              matrices[i], fri_.log_blowup,
+          math::RowMajorMatrix<F>& mat = matrices[i];
+          CHECK_EQ(coset.domain()->size(), static_cast<size_t>(mat.rows()));
+          math::RowMajorMatrix<F> lde(mat.rows() << fri_.log_blowup,
+                                      mat.cols());
+          coset.domain()->CosetLDEBatch(
+              mat, fri_.log_blowup,
               F::FromMontgomery(F::Config::kSubgroupGenerator) *
                   coset.domain()->offset_inv(),
-              /*reverse_at_last=*/false);
+              lde, /*reverse_at_last=*/false);
+          return lde;
         });
     return mmcs_.CommitOwned(std::move(ldes), commitment, prover_data);
   }

tachyon/math/polynomials/univariate/naive_batch_fft.h
@@ -57,29 +57,27 @@ class NaiveBatchFFT : public TwoAdicSubgroup<NaiveBatchFFT<F>> {
   }
 
   // Compute the low-degree extension of each column in |mat| onto a coset of
-  // a larger subgroup.
+  // a larger subgroup and populate |out| with the result.
   template <typename Derived>
-  RowMajorMatrix<F> CosetLDEBatch(Eigen::MatrixBase<Derived>& mat,
-                                  size_t added_bits, F shift) const {
+  void CosetLDEBatch(Eigen::MatrixBase<Derived>& mat, size_t added_bits,
+                     F shift, Eigen::MatrixBase<Derived>& out) const {
     if constexpr (F::Config::kModulusBits > 32) {
       NOTREACHED();
     }
-    this->IFFTBatch(mat);
     Eigen::Index rows = mat.rows();
     Eigen::Index new_rows = rows << added_bits;
-    Eigen::Index cols = mat.cols();
+    CHECK_EQ(out.rows(), new_rows);
+    CHECK_EQ(out.cols(), mat.cols());
+    this->IFFTBatch(mat);
 
-    // Possible crash if the new resized length overflows
-    RowMajorMatrix<F> ret(new_rows, cols);
     OMP_PARALLEL_FOR(Eigen::Index row = 0; row < new_rows; ++row) {
       if (row < rows) {
-        ret.row(row) = mat.row(row);
+        out.row(row) = mat.row(row);
       } else {
-        ret.row(row).setZero();
+        out.row(row).setZero();
       }
     }
-    CosetFFTBatch(ret, shift);
-    return ret;
+    CosetFFTBatch(out, shift);
   }
 
  private:

tachyon/math/polynomials/univariate/radix2_evaluation_domain.h
@@ -118,10 +118,10 @@ class Radix2EvaluationDomain
     ReverseMatrixIndexBits(mat);
   }
 
-  template <typename Derived>
-  CONSTEXPR_IF_NOT_OPENMP RowMajorMatrix<F> CosetLDEBatch(
+  template <typename Derived, typename Derived2>
+  CONSTEXPR_IF_NOT_OPENMP void CosetLDEBatch(
       Eigen::MatrixBase<Derived>& mat, size_t added_bits, F shift,
-      bool reverse_at_last = true) const {
+      Eigen::MatrixBase<Derived2>& out, bool reverse_at_last = true) const {
     TRACE_EVENT("EvaluationDomain", "Radix2EvaluationDomain::CosetLDEBatch");
     if constexpr (F::Config::kModulusBits > 32) {
       NOTREACHED();
@@ -162,15 +162,13 @@ class Radix2EvaluationDomain
                                          this->log_size_of_group_)) *= weight;
     });
 
-    RowMajorMatrix<F> ret;
     if (added_bits == 0) {
-      ret = mat;
+      out = mat;
     } else {
-      ret = RowMajorMatrix<F>(this->size_ << added_bits, mat.cols());
-      ExpandWithZeroPad(mat, added_bits, ret);
+      ExpandWithZeroPad(mat, added_bits, out);
     }
 
-    size_t rows = static_cast<size_t>(ret.rows());
+    size_t rows = static_cast<size_t>(out.rows());
     uint32_t log_size_of_group = base::bits::CheckedLog2(rows);
     auto domain =
         absl::WrapUnique(new Radix2EvaluationDomain(rows, log_size_of_group));
@@ -178,17 +176,16 @@ class Radix2EvaluationDomain
         Radix2TwiddleCache<F>::GetItem(domain.get(), /*packed_vec_only=*/true);
 
     // The first half looks like a normal DIT.
-    domain->RunParallelRowChunks(ret, domain->cache_->roots_vec.back(),
+    domain->RunParallelRowChunks(out, domain->cache_->roots_vec.back(),
                                  domain->cache_->packed_roots_vec[0]);
 
     // For the second half, we flip the DIT, working in bit-reversed order.
-    ReverseMatrixIndexBits(ret);
-    domain->RunParallelRowChunksReversed(ret, domain->cache_->rev_roots_vec,
+    ReverseMatrixIndexBits(out);
+    domain->RunParallelRowChunksReversed(out, domain->cache_->rev_roots_vec,
                                          domain->cache_->packed_roots_vec[1]);
     if (reverse_at_last) {
-      ReverseMatrixIndexBits(ret);
+      ReverseMatrixIndexBits(out);
     }
-    return ret;
   }
 
  private:

tachyon/math/polynomials/univariate/radix2_evaluation_domain_unittest.cc
@@ -52,21 +52,23 @@ TYPED_TEST(Radix2EvaluationDomainTest, CosetLDEBatchDeath) {
       Radix2EvaluationDomain<F>::Create(1);
   RowMajorMatrix<F> matrix = RowMajorMatrix<F>::Random(1, 3);
   F shift = F::FromMontgomery(F::Config::kSubgroupGenerator);
-  EXPECT_DEATH(domain->CosetLDEBatch(matrix, 1, shift), "");
+  RowMajorMatrix<F> result(2, 3);
+  EXPECT_DEATH(domain->CosetLDEBatch(matrix, 1, shift, result), "");
 }
 
 TYPED_TEST(Radix2EvaluationDomainTest, CosetLDEBatch) {
   using F = TypeParam;
   for (uint32_t log_r = 1; log_r < 5; ++log_r) {
-    RowMajorMatrix<F> expected =
-        RowMajorMatrix<F>::Random(size_t{1} << log_r, 3);
-    RowMajorMatrix<F> result = expected;
+    RowMajorMatrix<F> input = RowMajorMatrix<F>::Random(size_t{1} << log_r, 3);
+    RowMajorMatrix<F> input_clone = input;
     NaiveBatchFFT<F> naive;
     F shift = F::FromMontgomery(F::Config::kSubgroupGenerator);
-    expected = naive.CosetLDEBatch(expected, 1, shift);
+    RowMajorMatrix<F> expected(size_t{1} << (log_r + 1), 3);
+    naive.CosetLDEBatch(input, 1, shift, expected);
     std::unique_ptr<Radix2EvaluationDomain<F>> domain =
         Radix2EvaluationDomain<F>::Create(size_t{1} << log_r);
-    result = domain->CosetLDEBatch(result, 1, shift);
+    RowMajorMatrix<F> result(size_t{1} << (log_r + 1), 3);
+    domain->CosetLDEBatch(input_clone, 1, shift, result);
     EXPECT_EQ(expected, result);
   }
 }

commit eb1d5e1bf5bcd5c5d66384b6963b86077a1c6d48

fix(benchmark): fix FFT batch benchmark timing error
Broken from db5ee0a

benchmark/fft_batch/fft_batch_runner.h
@@ -35,16 +35,18 @@ class FFTBatchRunner {
     math::RowMajorMatrix<F> result;
     std::unique_ptr<Domain> domain =
         Domain::Create(static_cast<size_t>(input.rows()));
-    base::TimeTicks start = base::TimeTicks::Now();
+    base::TimeTicks start;
     if (run_coset_lde) {
       const size_t kAddedBits = 1;
       result =
           math::RowMajorMatrix<F>(input.rows() << kAddedBits, input.cols());
+      start = base::TimeTicks::Now();
       domain->CosetLDEBatch(input, kAddedBits,
                             F::FromMontgomery(F::Config::kSubgroupGenerator),
                             result);
     } else {
       result = input;
+      start = base::TimeTicks::Now();
       domain->FFTBatch(result);
     }
     reporter_.AddTime(vendor, base::TimeTicks::Now() - start);